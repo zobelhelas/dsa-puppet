24431491f819f2bf1533bbb219f2c1ca489ea9f7
Stephen Gran <steve@lobefin.net>
2009-02-24 16:42:53 +0000

    Add some apt autoconfig magic Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/debian-org/manifests/init.pp
+++ modules/debian-org/manifests/init.pp
@@ -18,5 +18,22 @@ class debian-org {
              "mtr-tiny": ensure => latest;
              "nload": ensure => latest;
    }
+   file {
+      "/etc/apt/sources.list.d/backports.org.list":
+             source => "puppet:///files/etc/apt/sources.list.d/backports.org.list";
+
+      "/etc/apt/sources.list.d/debian.org.list":
+             source => "puppet:///files/etc/apt/sources.list.d/debian.org.list";
+
+      "/etc/apt/sources.list.d/security.list":
+             source => "puppet:///files/etc/apt/sources.list.d/security.list";
+
+      "/etc/apt/sources.list.d/volatile.list":
+             source => "puppet:///files/etc/apt/sources.list.d/volatile.list";
+      "/etc/apt/apt.conf.d/local-recommends":
+             source => "puppet:///files/etc/apt/apt.conf.d/local-recommends";
+   }
 }
+
+
 
