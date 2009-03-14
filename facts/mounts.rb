16585b6e58da75d74b213b22309d03fabba9b05c
Stephen Gran <steve@lobefin.net>
2009-03-14 20:32:22 +0000

    A mounts fact.  This will hopefully let us compare what we currently monitor to what is actually on the system Signed-off-by: Stephen Gran <steve@lobefin.net>

---
+++ facts/mounts.rb
@@ -0,0 +1,17 @@
+require 'filesystem'
+
+Facter.add("mounts") do
+	ignorefs = ["NFS", "nfs", "nfs4", "afs", "binfmt_misc", "proc", "smbfs", 
+		    "autofs", "iso9660", "ncpfs", "coda", "devpts", "ftpfs", "devfs", 
+		    "mfs", "shfs", "sysfs", "cifs", "lustre_lite", "tmpfs", "usbfs", "udf"]
+	mountpoints = []
+	FileSystem.mounts.each do |m|
+		if not ignorefs.include?(m.fstype) && m.options !~ /bind/
+			mountpoints << m.mount
+		end
+	end
+	setcode do
+		mountpoints.join(',')
+	end
+end
+
