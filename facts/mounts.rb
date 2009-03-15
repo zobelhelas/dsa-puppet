38e8d2cb048d655050ef69e16756231222967987
Stephen Gran <steve@lobefin.net>
2009-03-15 00:42:49 +0000

    And another Signed-off-by: Stephen Gran <steve@lobefin.net>

--- facts/mounts.rb
+++ facts/mounts.rb
@@ -2,7 +2,7 @@ begin
   require 'filesystem'
 
 	Facter.add("mounts") do
-		ignorefs = ["NFS", "nfs", "nfs4", "afs", "binfmt_misc", "proc", "smbfs", 
+		ignorefs = ["NFS", "nfs", "nfs4", "nfsd", "afs", "binfmt_misc", "proc", "smbfs", 
 			    "autofs", "iso9660", "ncpfs", "coda", "devpts", "ftpfs", "devfs", 
 			    "mfs", "shfs", "sysfs", "cifs", "lustre_lite", "tmpfs", "usbfs", "udf",
 			    "fusectl", "fuse.snapshotfs", "rpc_pipefs"]
