827241b81f736199464e08b2e772714754a15b81
Peter Palfrader <peter@palfrader.org>
2009-03-31 20:19:56 +0200

    First cut at nagios client

---
+++ modules/nagios/manifests/init.pp
@@ -0,0 +1,40 @@
+class nagios-client {
+	package {
+		nagios-nrpe-server: ensure => installed,
+		dsa-nagios-checks: ensure => installed
+	}
+
+	file {
+		"/etc/default/nagios-nrpe-server":
+			source  => [ "puppet:///nagios/per-host/$fqdn/default",
+			             "puppet:///nagios/common/default" ],
+			require => Package["nagios-nrpe-server"],
+			notify  => Exec["nagios-nrpe-server restart"];
+		"/etc/default/nagios-nrpe":
+			ensure  => absent,
+			notify  => Exec["nagios-nrpe-server restart"];
+		"/etc/nagios/nrpe.cfg":
+			source  => [ "puppet:///nagios/per-host/$fqdn/nrpe.cfg",
+			             "puppet:///nagios/common/nrpe.cfg" ],
+			require => Package["nagios-nrpe-server"],
+			notify  => Exec["nagios-nrpe-server restart"];
+		"/etc/nagios/nrpe.cfg":
+			source  => [ "puppet:///nagios/per-host/$fqdn/nrpe.cfg",
+			             "puppet:///nagios/common/nrpe.cfg" ],
+			require => Package["nagios-nrpe-server"],
+			notify  => Exec["nagios-nrpe-server restart"];
+		"/etc/nagios/nrpe.d":
+			mode    => 755,
+			ensure  => directory;
+		"/etc/nagios/nrpe.d/nrpe_dsa.cfg":
+			source  => [ "puppet:///nagios/dsa-nagios/generated/nrpe_dsa.cfg" ],
+			require => Package["nagios-nrpe-server"],
+			notify  => Exec["nagios-nrpe-server restart"];
+	}
+
+	exec { "nagios-nrpe-server restart":
+		path        => "/etc/init.d:/usr/bin:/usr/sbin:/bin:/sbin",
+		refreshonly => true,
+	}
+}
+
