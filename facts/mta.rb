d21074d0a77d36f09ea70fbaa8cca30b94bdb2e5
Stephen Gran <steve@lobefin.net>
2009-04-05 02:09:02 +0100

    Make TLS usage in exim conditional on having the infrastructure to do so.  It's a little racy, so I don't want to make exim whine about it Signed-off-by: Stephen Gran <steve@lobefin.net>

--- facts/mta.rb
+++ facts/mta.rb
@@ -7,3 +7,13 @@ Facter.add("mta") do
 		mta
 	end
 end
+
+facter.add("exim_ssl_certs") do
+	setcode do
+		FileTest.exist?("/etc/exim4/ssl/") &&
+		FileTest.exist?("/etc/exim4/ssl/ca.crl") &&
+		FileTest.exist?("/etc/exim4/ssl/thishost.crt") &&
+		FileTest.exist?("/etc/exim4/ssl/ca.crt") &&
+		FileTest.exist?("/etc/exim4/ssl/thishost.key")
+	end
+end
