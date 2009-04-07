a03c85261ca9724f599adf09d3b4b731200b0731
Peter Palfrader <peter@palfrader.org>
2009-04-08 01:13:57 +0200

    Add logrotate config for apache

--- modules/apache2/manifests/init.pp
+++ modules/apache2/manifests/init.pp
@@ -15,6 +15,11 @@ class apache2 {
 			             "puppet:///apache2/common/etc/apache2/sites-available/default-debian.org" ],
 			require => Package["apache2"],
 			notify  => Exec["apache2 reload"];
+
+		"/etc/logroate.d/apache2":
+			source  => [ "puppet:///apache2/per-host/$fqdn/etc/logroate.d/apache2",
+			             "puppet:///apache2/common/etc/logroate.d/apache2" ];
+
 		"/srv/www":
 			mode    => 755,
 			ensure  => directory;
@@ -26,6 +31,10 @@ class apache2 {
 			ensure  => directory;
 		"/srv/www/default.debian.org/htdocs/index.html":
 			content => template("default-index.html");
+
+		"/var/log/apache2":
+			mode    => 755,
+			ensure  => directory;
 	}
 
 	exec { "apache2 reload":
