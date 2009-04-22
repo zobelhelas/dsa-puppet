a85df2bee6c3751cb0ebe0a382ddd819da7e7f44
Stephen Gran <steve@lobefin.net>
2009-04-23 00:12:15 +0100

    Rebuild motd when the tail changes Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/motd/manifests/init.pp
+++ modules/motd/manifests/init.pp
@@ -1,5 +1,9 @@
 class motd {
 	file { "/etc/motd.tail":
+                notify  => Exec["updatemotd"],
                 content => template("motd.erb") ;
 	}
+        exec { "updatemotd":
+                command => "uname -snrvm > /var/run/motd && cat /etc/motd.tail >> /var/run/motd"
+        }
 }
