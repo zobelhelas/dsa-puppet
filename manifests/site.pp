e931e24ea345240512fad403c6e17ebb3789c8c2
Stephen Gran <steve@lobefin.net>
2009-04-26 16:00:14 +0100

    Revert "notice?"
    This reverts commit 7a82591e6beeaa212dc23815c42349ca4fe08e4d.

--- manifests/site.pp
+++ manifests/site.pp
@@ -22,7 +22,7 @@ node default {
     include samhain
 
     $nodeinfo = nodeinfo($fqdn, "/home/sgran/local.yaml")
-    notice($nodeinfo)
+    debug($nodeinfo)
 
     case $smartarraycontroller {
         "true":    { include debian-proliant }
