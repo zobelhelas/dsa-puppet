00cd31ec62c1c1ad43adf0ec7e18a71327c3fc58
Stephen Gran <steve@lobefin.net>
2009-04-26 16:12:07 +0100

    Lets not blow things up for everybody Signed-off-by: Stephen Gran <steve@lobefin.net>

--- manifests/site.pp
+++ manifests/site.pp
@@ -21,8 +21,6 @@ node default {
     include samhain
 
     $nodeinfo = nodeinfo($fqdn, "/home/sgran/local.yaml")
-    notice($nodeinfo.class)
-    notice($nodeinfo)
 
     case $smartarraycontroller {
         "true":    { include debian-proliant }
--- modules/motd/manifests/new.pp
+++ modules/motd/manifests/new.pp
@@ -1,4 +1,8 @@
 class motd::new inherits motd {
+        $str = $nodeinfo.class
+        notice($str)
+        notice($nodeinfo)
+
 	file { "/etc/motd.tail":
                 notify  => Exec["updatemotd"],
                 content => template("motd-new.erb") ;
