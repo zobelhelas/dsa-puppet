a036864926be3a40a79b5c930bcce1e66f3f091a
Peter Palfrader <peter@palfrader.org>
2009-04-26 19:11:06 +0200

    Add an apt-keys module

---
+++ modules/apt-keys/manifests/init.pp
@@ -0,0 +1,21 @@
+class apt-keys {
+    file {
+        "/etc/apt/trusted-keys.d/":
+          ensure  => directory,
+          purge   => true,
+          notify  => Exec["updatekeys"],
+          ;
+
+        "/etc/apt/trusted-keys.d/db.debian.org.asc":
+          source  => "puppet:///apt-keys/db.debian.org.asc",
+          mode    => 664,
+          notify  => Exec["updatekeys"],
+          ;
+    }
+
+    exec { "updatekeys":
+         command => "for keyfile in /etc/apt/trusted-keys.d/*; do apt-key add $keyfile; done",
+         refreshonly => true
+    }
+}
+
