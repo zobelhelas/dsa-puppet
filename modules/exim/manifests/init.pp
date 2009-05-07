dac3b98b327149bf1458d77bec32a77ce4facc2c
Stephen Gran <steve@lobefin.net>
2009-05-07 07:49:05 +0100

    last service host Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/exim/manifests/init.pp
+++ modules/exim/manifests/init.pp
@@ -3,7 +3,7 @@ class exim {
     package { exim4-daemon-heavy: ensure => installed }
 
     case $hostname {
-         handel, kassia, piatti, spohr, ancina, allegri: {
+         handel, kassia, piatti, spohr, ancina, allegri, powell: {
              file {
                 "/etc/exim4/exim4.conf":
                   content => template("exim/eximconf.erb"),
