a6a806b9918fc786b2c2e06a8b5c13cc522aea14
Stephen Gran <steve@lobefin.net>
2009-05-07 01:35:51 +0100

    Add spohr Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/exim/manifests/init.pp
+++ modules/exim/manifests/init.pp
@@ -3,7 +3,7 @@ class exim {
     package { exim4-daemon-heavy: ensure => installed }
 
     case $hostname {
-         handel, kassia, piatti: {
+         handel, kassia, piatti, spohr: {
              file {
                 "/etc/exim4/exim4.conf":
                   content => template("exim/eximconf.erb"),
