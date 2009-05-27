76cb048af5009b827b27745dcf46e7503b4b7944
Peter Palfrader <peter@palfrader.org>
2009-05-27 21:24:00 +0200

    Add recvconf, recvconf.files, and auth keys to manifest

--- modules/geodns/manifests/init.pp
+++ modules/geodns/manifests/init.pp
@@ -35,11 +35,40 @@ class geodns {
                         ;
                 "/etc/bind/named.conf.options":
                         source  => [ "puppet:///geodns/per-host/$fqdn/named.conf.options",
-                                      "puppet:///geodns/common/named.conf.options" ],
+                                     "puppet:///geodns/common/named.conf.options" ],
                         require => Package["bind9"],
                         notify  => Exec["bind9 restart"],
                         owner   => root,
                         group   => root,
+                        ;
+
+                "/etc/bind/geodns":
+                        ensure  => directory,
+                        owner   => root,
+                        group   => geodnssync,
+                        mode    => 775,
+                        ;
+                "/etc/bind/geodns/recvconf":
+                        source  => [ "puppet:///geodns/per-host/$fqdn/recvconf",
+                                     "puppet:///geodns/common/recvconf" ],
+                        owner   => root,
+                        group   => root,
+                        mode    => 555,
+                        ;
+                "/etc/bind/geodns/recvconf.files":
+                        source  => [ "puppet:///geodns/per-host/$fqdn/recvconf.files",
+                                     "puppet:///geodns/common/recvconf.files" ],
+                        owner   => root,
+                        group   => root,
+                        mode    => 444,
+                        ;
+
+                "/etc/ssh/userkeys/geodnssync":
+                        source  => [ "puppet:///geodns/per-host/$fqdn/authorized_keys",
+                                     "puppet:///geodns/common/authorized_keys" ],
+                        owner   => root,
+                        group   => geodnssync,
+                        mode    => 440,
                         ;
         }
 
