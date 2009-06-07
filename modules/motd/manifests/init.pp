4a7dded7abc6da1697eb759aa8ee7a0d289f02f5
Stephen Gran <steve@lobefin.net>
2009-06-07 18:44:10 +0100

    Let's try a symlink Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/motd/manifests/init.pp
+++ modules/motd/manifests/init.pp
@@ -2,6 +2,8 @@ class motd {
 	file { "/etc/motd.tail":
                 notify  => Exec["updatemotd"],
                 content => template("motd/motd.erb") ;
+               "/etc/motd":
+                 ensure => "/var/run/motd";
 	}
         exec { "updatemotd":
                 command => "uname -snrvm > /var/run/motd && cat /etc/motd.tail >> /var/run/motd",
