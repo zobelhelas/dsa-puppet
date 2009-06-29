a8a2c4473311f88784e5edac68ea4fe0074b0c90
Stephen Gran <steve@lobefin.net>
2009-06-29 08:03:30 +0100

    How many of our hosts have a broken /etc/hosts ? Signed-off-by: Stephen Gran <steve@lobefin.net>

---
+++ facts/hosts.rb
@@ -0,0 +1,19 @@
+Facter.add("brokenhosts") do
+	confine :kernel => :linux
+	brokenhosts = "true"
+	if FileTest.exist?("/etc/hosts")
+		IO.foreach("/etc/hosts") do |x|
+			x.split.each do |y|
+				if y == fqdn
+					brokenhosts = "false"
+					break
+				end
+			end
+		end
+	end
+	setcode do
+		brokenhosts == "true"
+	end
+end
+
+
