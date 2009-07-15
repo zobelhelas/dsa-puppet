f72074b094219c67c9a77ff84eda49885ded97b9
Stephen Gran <steve@lobefin.net>
2009-07-15 12:58:32 +0100

    Manage clamav-unofficial-sigs.  The dsa.conf removes all dbs marked as high risk of false positives Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/clamav/manifests/init.pp
+++ modules/clamav/manifests/init.pp
@@ -3,5 +3,15 @@ class clamav {
               "clamav-freshclam": ensure => installed;
               "clamav-unofficial-sigs": ensure => installed;
     }
+    file {
+        "/etc/clamav-unofficial-sigs.dsa.conf":
+          require => Package["clamav-unofficial-sigs"],
+          source  => [ "puppet:///clamav/clamav-unofficial-sigs.dsa.conf" ]
+          ;
+        "/etc/clamav-unofficial-sigs.conf":
+          require => Package["clamav-unofficial-sigs"],
+          source  => [ "puppet:///clamav/clamav-unofficial-sigs.conf" ]
+          ;
+    }
 }
 
