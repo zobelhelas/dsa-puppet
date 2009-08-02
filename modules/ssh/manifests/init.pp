160b78150b3f5a2d16c9e67bf5c5a991598bd6c2
Stephen Gran <steve@lobefin.net>
2009-08-02 01:18:08 +0100

    Restart ssh when config file changes Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/ssh/manifests/init.pp
+++ modules/ssh/manifests/init.pp
@@ -10,7 +10,13 @@ class ssh {
                 ;
 	       "/etc/ssh/sshd_config":
 		content => template("ssh/sshd_config.erb"),
-		require => Package["openssh-server"]
+		require => Package["openssh-server"],
+                notify  => Exec["ssh restart"]
                 ;
 	}
+
+        exec { "ssh restart":
+            path        => "/etc/init.d:/usr/bin:/usr/sbin:/bin:/sbin",
+            refreshonly => true,
+        }
 }
