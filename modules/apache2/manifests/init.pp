37978abc1d086b93de416cbd89ec80c1547dc1b3
Peter Palfrader <peter@palfrader.org>
2009-08-23 10:31:00 +0200

    set apache ressource limits (RT#1481)

--- modules/apache2/manifests/init.pp
+++ modules/apache2/manifests/init.pp
@@ -5,6 +5,11 @@ class apache2 {
 	}
 
 	file {
+		"/etc/apache2/conf.d/ressource-limits":
+			source  => [ "puppet:///apache2/per-host/$fqdn/etc/apache2/conf.d/ressource-limits",
+			             "puppet:///apache2/common/etc/apache2/conf.d/ressource-limits" ],
+			require => Package["apache2"],
+			notify  => Exec["apache2 reload"];
 		"/etc/apache2/conf.d/security":
 			source  => [ "puppet:///apache2/per-host/$fqdn/etc/apache2/conf.d/security",
 			             "puppet:///apache2/common/etc/apache2/conf.d/security" ],
