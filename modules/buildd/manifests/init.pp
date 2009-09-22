0485bf5797d3a45c9bebcd9f1be9ec61b85f853c
Stephen Gran <steve@lobefin.net>
2009-09-23 00:01:16 +0100

    and move buildd list to its module
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/buildd/manifests/init.pp
+++ modules/buildd/manifests/init.pp
@@ -9,7 +9,7 @@ class buildd {
 
    file {
       "/etc/apt/sources.list.d/buildd.list":
-             source => "puppet:///files/etc/apt/sources.list.d/buildd.list",
+             source  => template("buildd/etc/apt/sources.list.d/buildd.list"),
              require => Package["apt-transport-https"],
              notify  => Exec["apt-get update"],
              ;
