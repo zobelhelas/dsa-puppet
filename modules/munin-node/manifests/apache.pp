547b76eb08936349757a71f00b537af987b86bac
Stephen Gran <steve@lobefin.net>
2009-10-16 21:39:06 +0100

    maybe?
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/munin-node/manifests/apache.pp
+++ modules/munin-node/manifests/apache.pp
@@ -1,6 +1,8 @@
 class munin-node::apache inherits munin-node {
-        activate_munin_check { "apache_accesses" }
-        activate_munin_check { "apache_processes" }
-        activate_munin_check { "apache_volume" }
+        activate_munin_check { "Apache munin plugins:
+                script => "apache_accesses";
+                script => "apache_processes";
+                script => "apache_volume";
+        }
 }
 
