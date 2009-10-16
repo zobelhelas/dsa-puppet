bcc6496b060e594843dc8cd61f3e1013acc4edce
Stephen Gran <steve@lobefin.net>
2009-10-16 21:15:25 +0100

    apache hosts load mod info and status, and serve it
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/apache2/manifests/init.pp
+++ modules/apache2/manifests/init.pp
@@ -1,8 +1,17 @@
+define enable_module($module) {
+        exec { "/usr/sbin/a2enmod $module": }
+}
+
 class apache2 {
 	package {
 		apache2: ensure => installed;
 		logrotate: ensure => installed;
 	}
+
+        enable_module {
+                "info": module => info;
+                "status": module => status;
+        }
 
 	file {
 		"/etc/apache2/conf.d/ressource-limits":
@@ -12,6 +21,15 @@ class apache2 {
 		"/etc/apache2/conf.d/security":
 			source  => [ "puppet:///apache2/per-host/$fqdn/etc/apache2/conf.d/security",
 			             "puppet:///apache2/common/etc/apache2/conf.d/security" ],
+			require => Package["apache2"],
+			notify  => Exec["apache2 reload"];
+		"/etc/apache2/conf.d/local-serverinfo":
+			source  => [ "puppet:///apache2/per-host/$fqdn/etc/apache2/conf.d/local-serverinfo",
+			             "puppet:///apache2/common/etc/apache2/conf.d/local-serverinfo" ],
+			require => Package["apache2"],
+		"/etc/apache2/conf.d/server-status":
+			source  => [ "puppet:///apache2/per-host/$fqdn/etc/apache2/conf.d/server-status",
+			             "puppet:///apache2/common/etc/apache2/conf.d/server-status" ],
 			require => Package["apache2"],
 			notify  => Exec["apache2 reload"];
 
