4b8bd3b11bf706f3cfdcf8e07e4f5abda3b93750
Stephen Gran <steve@lobefin.net>
2009-10-16 22:58:25 +0100

    some default munin plugins for everyone
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/munin-node/manifests/init.pp
+++ modules/munin-node/manifests/init.pp
@@ -9,6 +9,25 @@ class munin-node {
 
     package { munin-node: ensure => installed }
 
+    activate_munin_check {
+        "cpu":;
+        "df":;
+        "df_inode":;
+        "entropy":;
+        "forks":;
+        "interrupts":;
+        "iostat":;
+        "irqstats":;
+        "load":;
+        "memory":;
+        "ntp_offset":;
+        "open_files":;
+        "open_inodes":;
+        "processes":;
+        "swap":;
+        "vmstat":;
+    }
+
     file { "/etc/munin/munin-node.conf":
         source  => [ "puppet:///munin-node/per-host/$fqdn/munin-node.conf",
                      "puppet:///munin-node/common/munin-node.conf" ],
