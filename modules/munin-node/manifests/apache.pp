f9f0db58c75987e081d0eecd10baef49d2c800f4
Stephen Gran <steve@lobefin.net>
2009-10-16 21:28:18 +0100

    right.  actually decalre inheritance
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/munin-node/manifests/apache.pp
+++ modules/munin-node/manifests/apache.pp
@@ -1,4 +1,4 @@
-class munin-node::apache {
+class munin-node::apache inherits munin-node {
         activate_munin_check("apache_accesses");
         activate_munin_check("apache_processes");
         activate_munin_check("apache_volume");
