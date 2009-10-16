c10de1822ccaae5b03359458b9e5bb17d0ad8c21
Stephen Gran <steve@lobefin.net>
2009-10-16 21:40:19 +0100

    an array?  really?
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/munin-node/manifests/apache.pp
+++ modules/munin-node/manifests/apache.pp
@@ -1,8 +1,8 @@
 class munin-node::apache inherits munin-node {
         activate_munin_check { "Apache munin plugins:
-                script => "apache_accesses";
+              [ script => "apache_accesses";
                 script => "apache_processes";
-                script => "apache_volume";
+                script => "apache_volume"; ]
         }
 }
 
