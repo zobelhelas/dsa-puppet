e5c0f9f8fd186a77ad0b77611f06072613b7b44f
Stephen Gran <steve@lobefin.net>
2009-10-18 01:13:01 +0100

    and clean up the paths
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/named/manifests/geodns.pp
+++ modules/named/manifests/geodns.pp
@@ -5,24 +5,24 @@ class named::geodns inherits named {
                         notify  => Exec["apt-get update"],
                         ;
                 "/etc/bind/named.conf.local":
-                        source  => [ "puppet:///geodns/per-host/$fqdn/named.conf.local",
-                                     "puppet:///geodns/common/named.conf.local" ],
+                        source  => [ "puppet:///named/per-host/$fqdn/named.conf.local",
+                                     "puppet:///named/per-host/named.conf.local" ],
                         require => Package["bind9"],
                         notify  => Exec["bind9 restart"],
                         owner   => root,
                         group   => root,
                         ;
                 "/etc/bind/named.conf.acl":
-                        source  => [ "puppet:///geodns/per-host/$fqdn/named.conf.acl",
-                                     "puppet:///geodns/common/named.conf.acl" ],
+                        source  => [ "puppet:///named/per-host/$fqdn/named.conf.acl",
+                                     "puppet:///named/per-host/named.conf.acl" ],
                         require => Package["bind9"],
                         notify  => Exec["bind9 restart"],
                         owner   => root,
                         group   => root,
                         ;
                 "/etc/bind/named.conf.options":
-                        source  => [ "puppet:///geodns/per-host/$fqdn/named.conf.options",
-                                     "puppet:///geodns/common/named.conf.options" ],
+                        source  => [ "puppet:///named/per-host/$fqdn/named.conf.options",
+                                     "puppet:///named/per-host/named.conf.options" ],
                         require => Package["bind9"],
                         notify  => Exec["bind9 restart"],
                         owner   => root,
@@ -36,39 +36,39 @@ class named::geodns inherits named {
                         mode    => 775,
                         ;
                 "/etc/bind/geodns/named.conf.geo":
-                        source  => [ "puppet:///geodns/per-host/$fqdn/named.conf.geo",
-                                     "puppet:///geodns/common/named.conf.geo" ],
+                        source  => [ "puppet:///named/per-host/$fqdn/named.conf.geo",
+                                     "puppet:///named/per-host/named.conf.geo" ],
                         require => Package["bind9"],
                         notify  => Exec["bind9 restart"],
                         owner   => root,
                         group   => root,
                         ;
                 "/etc/bind/geodns/recvconf":
-                        source  => [ "puppet:///geodns/per-host/$fqdn/recvconf",
-                                     "puppet:///geodns/common/recvconf" ],
+                        source  => [ "puppet:///named/per-host/$fqdn/recvconf",
+                                     "puppet:///named/per-host/recvconf" ],
                         owner   => root,
                         group   => root,
                         mode    => 555,
                         ;
                 "/etc/bind/geodns/recvconf.files":
-                        source  => [ "puppet:///geodns/per-host/$fqdn/recvconf.files",
-                                     "puppet:///geodns/common/recvconf.files" ],
+                        source  => [ "puppet:///named/per-host/$fqdn/recvconf.files",
+                                     "puppet:///named/per-host/recvconf.files" ],
                         owner   => root,
                         group   => root,
                         mode    => 444,
                         ;
 
                 "/usr/share/GeoIP/GeoIPv6.dat":
-                        source  => [ "puppet:///geodns/per-host/$fqdn/GeoIPv6.dat",
-                                     "puppet:///geodns/common/GeoIPv6.dat" ],
+                        source  => [ "puppet:///named/per-host/$fqdn/GeoIPv6.dat",
+                                     "puppet:///named/per-host/GeoIPv6.dat" ],
                         owner   => root,
                         group   => root,
                         mode    => 444,
                         ;
 
                 "/etc/ssh/userkeys/geodnssync":
-                        source  => [ "puppet:///geodns/per-host/$fqdn/authorized_keys",
-                                     "puppet:///geodns/common/authorized_keys" ],
+                        source  => [ "puppet:///named/per-host/$fqdn/authorized_keys",
+                                     "puppet:///named/per-host/authorized_keys" ],
                         owner   => root,
                         group   => geodnssync,
                         mode    => 440,
--- modules/named/manifests/secondary.pp
+++ modules/named/manifests/secondary.pp
@@ -1,7 +1,7 @@
 class named::secondary inherits named {
 #    file { "/etc/bind/named.conf.debian-zones":
-#        source  => [ "puppet:///named-secondary/per-host/$fqdn/named.conf.debian-zones",
-#                     "puppet:///named-secondary/common/named.conf.debian-zones" ],
+#        source  => [ "puppet:///named/per-host/$fqdn/named.conf.debian-zones",
+#                     "puppet:///named/common/named.conf.debian-zones" ],
 #        notify  => Exec["bind9 reload"],
 #    }
 }
