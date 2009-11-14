1903a9228b1e0f976ec29df3ffefe9b9c26ba653
Stephen Gran <steve@lobefin.net>
2009-11-14 18:50:08 +0000

    And fixup the allnodeinfo usage Signed-off-by: Stephen Gran <steve@lobefin.net>

--- files/etc/puppet/lib/puppet/parser/functions/allnodeinfo.rb
+++ files/etc/puppet/lib/puppet/parser/functions/allnodeinfo.rb
@@ -1,6 +1,7 @@
 module Puppet::Parser::Functions
   newfunction(:allnodeinfo, :type => :rvalue) do |attributes|
     require '/etc/puppet/lib/puppet/parser/functions/ldapinfo.rb'
-    return (function_ldapinfo('*', attributes))
+    attributes.unshift('*')
+    return (function_ldapinfo(attributes))
   end
 end
