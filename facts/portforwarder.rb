3646f25af6afc011870db2370c53c8ac58a79188
Peter Palfrader <peter@palfrader.org>
2010-02-21 14:26:27 +0100

    Rename portforwarder to portforwarder-key; Add portforwarder-user-exists fact

--- facts/portforwarder.rb
+++ facts/portforwarder.rb
@@ -1,14 +1,32 @@
-Facter.add("portforwarder") do
-    setcode do
-        key = nil
-        keyfile = '/home/portforwarder/.ssh/id_rsa.pub'
-        if FileTest.exist?(keyfile)
-            key = File.open(keyfile).read.chomp
-        end
-        key
-    end
-end
+begin
+    require 'etc'
 
+    Facter.add("portforwarder-key") do
+        setcode do
+            key = nil
+            keyfile = '/home/portforwarder/.ssh/id_rsa.pub'
+            if FileTest.exist?(keyfile)
+                key = File.open(keyfile).read.chomp
+            end
+            key
+        end
+    end
+
+    Facter.add("portforwarder-user-exists") do
+        setcode do
+            result = false
+            begin
+                if Etc.getpwnam('portforwarder')
+                    result = true
+                end
+            rescue ArgumentError
+            end
+            result
+        end
+    end
+
+rescue Exception => e
+end
 # vim:set et:
 # vim:set ts=4:
 # vim:set shiftwidth=4:
