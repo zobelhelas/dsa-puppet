ab95643a2ad84e74ea08955ab3605d44263e1c2d
Stephen Gran <steve@lobefin.net>
2010-02-21 16:48:28 +0000

    Revert "will ferm do the right thing?"
    This reverts commit a47c22e58d6502bb314e9fca3814fae1cadc62e5.

--- modules/munin-node/manifests/init.pp
+++ modules/munin-node/manifests/init.pp
@@ -74,14 +74,9 @@ class munin-node {
         path        => "/etc/init.d:/usr/bin:/usr/sbin:/bin:/sbin",
         refreshonly => true,
     }
-    @ferm::rule { "dsa-munin-v4":
+    @ferm::rule { "dsa-munin":
             description     => "Allow munin from munin master",
-            rule            => "proto tcp mod state state (NEW) dport (munin) @subchain 'munin' { saddr (\$HOST_MUNIN_V4 \$HOST_NAGIOS_V4) ACCEPT; }"
-    }
-    @ferm::rule { "dsa-munin-v6":
-            description     => "Allow munin from munin master",
-            domain          => "ip6",
-            rule            => "proto tcp mod state state (NEW) dport (munin) @subchain 'munin' { saddr (\$HOST_MUNIN_V6 \$HOST_NAGIOS_V6) ACCEPT; }"
+            rule            => "proto tcp mod state state (NEW) dport (munin) @subchain 'munin' { saddr (\$HOST_MUNIN) ACCEPT; }"
     }
 }
 
