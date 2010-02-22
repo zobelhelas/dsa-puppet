0f847fe853e6be82ee582311b6f6879748ade1cc
Stephen Gran <steve@lobefin.net>
2010-02-22 01:08:03 +0000

    Revert "test classes"
    This reverts commit 9698ac16e7c372a59158f21f25aa2bbf8db0f020.

--- modules/nagios/manifests/init.pp
+++ modules/nagios/manifests/init.pp
@@ -2,8 +2,4 @@ class nagios {
 	package {
 		nagios-nrpe-server: ensure => installed;
 	}
-        file {
-                "/tmp/test":
-                        content => template("nagios/class-test.erb");
-        }
 }
