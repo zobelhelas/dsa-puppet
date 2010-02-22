9698ac16e7c372a59158f21f25aa2bbf8db0f020
Stephen Gran <steve@lobefin.net>
2010-02-22 00:35:02 +0000

    test classes
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/nagios/manifests/init.pp
+++ modules/nagios/manifests/init.pp
@@ -2,4 +2,8 @@ class nagios {
 	package {
 		nagios-nrpe-server: ensure => installed;
 	}
+        file {
+                "/tmp/test":
+                        content => template("nagios/class-test.erb");
+        }
 }
