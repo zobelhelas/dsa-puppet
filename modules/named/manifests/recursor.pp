7035eb98ec726aafc5dc6e68f22280ba8428a373
Stephen Gran <steve@lobefin.net>
2010-02-23 19:31:52 +0000

    name new class correctly
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/named/manifests/recursor.pp
+++ modules/named/manifests/recursor.pp
@@ -1,4 +1,4 @@
-class named::secondary inherits named {
+class named::recursor inherits named {
     file { "/etc/bind/named.conf.options":
         content => template("named/named.conf.options.erb"),
         notify  => Exec["bind9 reload"],
