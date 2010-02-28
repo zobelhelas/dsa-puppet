46275e646b84c1f6bad2673fc3d118fb22ec24bf
Peter Palfrader <peter@palfrader.org>
2010-02-28 21:06:44 +0100

    mpt has funny ears

---
+++ modules/raid-mpt/manifest/init.pp
@@ -0,0 +1,22 @@
+class raid-mpt {
+    package {
+        mtp-status: ensure => installed;
+    }
+
+    file {
+        "/etc/default/mpt-statusd":
+            content =>  "# This file is under puppet control" +
+                        "RUN_DAEMON=no",
+            notify  => Exec["mpt-statusd-stop"],
+            ;
+    }
+    exec {
+        "mpt-statusd-stop":
+            command: 'pidfile=/var/run/mpt-statusd.pid; ! [ -e "$pidfile" ] || /sbin/start-stop-daemon --oknodo --stop --quiet --pidfile "$pidfile"; rm -f "$pidfile"'
+            refreshonly => true,
+            ;
+    }
+}
+# vim:set et:
+# vim:set ts=4:
+# vim:set shiftwidth=4:
