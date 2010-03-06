66d50df2e34f639d28128f704423c3a81feb9599
Martin Zobel-Helas <zobel@debian.org>
2010-03-06 19:30:55 +0100

    remove obsolete modules

--- modules/ftp/manifests/init.pp
+++
@@ -1,9 +0,0 @@
-class ftp {
-
-    @ferm::rule { "dsa-rsync":
-            domain          => "(ip ip6)",
-            description     => "Allow rsync access",
-            rule            => "&SERVICE(tcp, 21)"
-    }
-
-}
