a39b229851b3ebd9349faa442f64935d47ab7195
Stephen Gran <steve@lobefin.net>
2010-03-06 13:36:00 +0000

    try in place array
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- manifests/site.pp
+++ manifests/site.pp
@@ -94,7 +94,7 @@ node default {
         piatti: {
            @ferm::rule { "dsa-udd-stunnel":
                description  => "port 8080 for udd stunnel",
-               rule         => "proto tcp saddr ( 192.25.206.16 70.103.162.29 217.196.43.134 ) dport 8080 ACCEPT;"
+               rule         => "&SERVICE_RANGE(tcp, http-alt, ( 192.25.206.16 70.103.162.29 217.196.43.134 )"
            }
         }
     }
