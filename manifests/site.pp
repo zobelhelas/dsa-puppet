79c1838a32fed5850e830ed35d557334e1dd8afc
Stephen Gran <steve@lobefin.net>
2010-03-07 23:35:26 +0000

    v6 rule is v6
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- manifests/site.pp
+++ manifests/site.pp
@@ -131,6 +131,7 @@ node default {
 		    rule            => "&SERVICE_RANGE(tcp, 8140, \$HOST_DEBIAN_V4)"
 	   }
 	   @ferm::rule { "dsa-puppet-v6":
+		    domain          => 'ip6',
 		    description     => "Allow puppet access",
 		    rule            => "&SERVICE_RANGE(tcp, 8140, \$HOST_DEBIAN_V6)"
 	   }
