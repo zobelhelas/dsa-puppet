6aa5f947f028d4492e2389b85e9121f3bc98c34f
Stephen Gran <steve@lobefin.net>
2010-04-04 13:35:38 +0100

    I don't know why confine doesn't work, but let's try to work around it
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- facts/ipaddresses.rb
+++ facts/ipaddresses.rb
@@ -1,26 +1,34 @@
 Facter.add("v4ips") do
         confine :kernel => :linux
         addrs = []
-        %x{ip addr list}.each do |line|
-                next unless line =~ /\s+inet/
-                next if line =~ /scope (link|host)/
-                if line =~ /\s+inet\s+(\S+)\/\d{1,2} .*/
-                        addrs << $1
+        if FileTest.exist?("/bin/ip")
+                %x{ip addr list}.each do |line|
+                        next unless line =~ /\s+inet/
+                        next if line =~ /scope (link|host)/
+                        if line =~ /\s+inet\s+(\S+)\/\d{1,2} .*/
+                                addrs << $1
+                        end
                 end
         end
+        ret = addrs.join(",")
+        if ret.empty?
+          ret = 'no'
+        end
         setcode do
-                addrs.join(",")
+          ret
         end
 end
 
 Facter.add("v6ips") do
         confine :kernel => :linux
         addrs = []
-        %x{ip addr list}.each do |line|
-                next unless line =~ /\s+inet/
-                next if line =~ /scope (link|host)/
-                if line =~ /\s+inet6\s+(\S+)\/\d{1,3} .*/
-                        addrs << $1
+        if FileTest.exist?("/bin/ip")
+                %x{ip addr list}.each do |line|
+                        next unless line =~ /\s+inet/
+                        next if line =~ /scope (link|host)/
+                        if line =~ /\s+inet6\s+(\S+)\/\d{1,3} .*/
+                                addrs << $1
+                        end
                 end
         end
         ret = addrs.join(",")
