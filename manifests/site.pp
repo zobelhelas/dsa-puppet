9976083ab498cf538ed2ea3a935ae0e073311f23
Martin Zobel-Helas <zobel@debian.org>
2010-07-25 00:05:50 +0200

    add agricola

--- manifests/site.pp
+++ manifests/site.pp
@@ -92,7 +92,7 @@ node default {
     }
 
     case $hostname {
-        franck,powell,logtest01,geo1,geo2,geo3,bartok,senfl,beethoven,piatti,saens,villa,lobos,raff,gluck,schein,wieck,steffani,ball,handel,tchaikovsky,heininen,nielsen,kokkonen,kaufmann: { include ferm }
+        franck,powell,logtest01,geo1,geo2,geo3,bartok,senfl,beethoven,piatti,saens,villa,lobos,raff,gluck,schein,wieck,steffani,ball,handel,tchaikovsky,heininen,nielsen,kokkonen,kaufmann,agricola: { include ferm }
     }
     case $hostname {
         zandonai,zelenka: {
