e63c8f520153204ce1bee4f4aca2b6eb0308a3f0
Stephen Gran <steve@lobefin.net>
2010-07-25 20:59:50 +0100

    try to start breaking up firewall rules into something not quite as messy
    Signed-off-by: Stephen Gran <steve@lobefin.net>

---
+++ modules/ferm/manifests/per-host.pp
@@ -0,0 +1,86 @@
+class ferm::per-host {
+
+    case $hostname {
+        ancina,zandonai,zelenka: {
+           include ferm::zivit
+        }
+    }
+    case $hostname {
+        franck,gluck,kaufmann,klecker,lobos,morricone,raff,ries,rietz,saens,schein,senfl,steffani,valente,villa,wieck: {
+           include ferm::rsync
+        }
+    }
+
+    case $hostname {
+        saens,villa,lobos,raff,gluck,schein,wieck,steffani,ries,rietz,franck,morricone,valente,klecker: {
+           include ferm::ftp
+        }
+    }
+
+    case $hostname {
+        piatti: {
+           @ferm::rule { "dsa-udd-stunnel":
+               description  => "port 8080 for udd stunnel",
+               rule         => "&SERVICE_RANGE(tcp, http-alt, ( 192.25.206.16 70.103.162.29 217.196.43.134 ))"
+           }
+        }
+
+        handel: {
+	   @ferm::rule { "dsa-puppet":
+		    description     => "Allow puppet access",
+		    rule            => "&SERVICE_RANGE(tcp, 8140, \$HOST_DEBIAN_V4)"
+	   }
+	   @ferm::rule { "dsa-puppet-v6":
+		    domain          => 'ip6',
+		    description     => "Allow puppet access",
+		    rule            => "&SERVICE_RANGE(tcp, 8140, \$HOST_DEBIAN_V6)"
+	   }
+        }
+	powell: {
+	   @ferm::rule { "dsa-powell-v6-tunnel":
+		    description     => "Allow powell to use V6 tunnel broker",
+		    rule            => "proto ipv6 saddr 212.227.117.6 jump ACCEPT"
+	   }
+	   @ferm::rule { "dsa-powell-btseed":
+                    domain          => "(ip ip6)",
+		    description     => "Allow powell to seed BT",
+		    rule            => "proto tcp dport 8000:8100 jump ACCEPT"
+	   }
+           @ferm::rule { "dsa-powell-rsync":
+                    description     => "Hoster wants to sync from here, and why not",
+                    rule            => "&SERVICE_RANGE(tcp, rsync, ( 195.20.242.90 192.25.206.33 82.195.75.106 206.12.19.118 ))"
+           }
+	}
+	beethoven: {
+	   @ferm::rule { "dsa-merikanto-beethoven":
+		    description     => "Allow merikanto",  # for nfs, and that uses all kind of ports by default.
+		    rule            => "source 172.22.127.147 interface bond0 jump ACCEPT",
+	   }
+	}
+	heininen: {
+	   @ferm::rule { "dsa-syslog":
+		    description     => "Allow syslog access",
+		    rule            => "&SERVICE_RANGE(tcp, 5140, \$HOST_DEBIAN_V4)"
+	   }
+	   @ferm::rule { "dsa-syslog-v6":
+		    domain          => 'ip6',
+		    description     => "Allow syslog access",
+		    rule            => "&SERVICE_RANGE(tcp, 5140, \$HOST_DEBIAN_V6)"
+	   }
+        }
+	kaufmann: {
+           @ferm::rule { "dsa-hkp":
+		    domain          => "(ip ip6)",
+		    description     => "Allow hkp access",
+		    rule            => "&SERVICE(tcp, 11371)"
+           }
+	}
+	liszt: {
+           @ferm::rule { "smtp":
+		    domain          => "(ip ip6)",
+		    description     => "Allow smtp access",
+		    rule            => "&SERVICE(tcp, 25)"
+           }
+        }
+    }
+}
