e7285b4d1c821aa18ec266021ea1e72b51684161
Martin Zobel-Helas <zobel@debian.org>
2010-07-25 17:53:43 +0200

    yes, liszt might want to speak smtp

--- manifests/site.pp
+++ manifests/site.pp
@@ -197,6 +197,13 @@ node default {
 		    rule            => "&SERVICE(tcp, 873)"
 	   }
 	}
+	liszt: {
+           @ferm::rule { "smtp":
+		    domain          => "(ip ip6)",
+		    description     => "Allow smtp access",
+		    rule            => "&SERVICE(tcp, 25)"
+           }
+        }
     }
     case $brokenhosts {
         "true":    { include hosts }
