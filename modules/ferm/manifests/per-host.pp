f7acea45df7b1572bdadd3cfeed9632cbe5c695d
Martin Zobel-Helas <zobel@debian.org>
2010-07-25 23:47:41 +0200

    remove chopin

--- modules/ferm/manifests/per-host.pp
+++ modules/ferm/manifests/per-host.pp
@@ -12,7 +12,7 @@ class ferm::per-host {
     }
 
     case $hostname {
-        chopin,saens,villa,lobos,raff,gluck,schein,wieck,steffani,ries,rietz,franck,morricone,valente,klecker: {
+        saens,villa,lobos,raff,gluck,schein,wieck,steffani,ries,rietz,franck,morricone,valente,klecker: {
            include ferm::ftp
         }
     }
