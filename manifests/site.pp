90c81693876544adc80b527598c57310a130b9f8
Martin Zobel-Helas <zobel@debian.org>
2010-07-25 18:01:38 +0200

    add liszt

--- manifests/site.pp
+++ manifests/site.pp
@@ -88,7 +88,7 @@ node default {
     case $hostname {
         klecker,ravel,senfl,orff: { include named::secondary }
         geo1,geo2,geo3:           { include named::geodns }
-        bartok,schein,steffani,ries,franck:   { include named::recursor }
+        bartok,schein,steffani,ries,franck,liszt:   { include named::recursor }
     }
 
     case $hostname {
