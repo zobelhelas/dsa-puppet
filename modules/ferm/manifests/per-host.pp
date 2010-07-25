88e5344fecee3a372f760ba53b94e824c33d5c4f
Martin Zobel-Helas <zobel@debian.org>
2010-07-25 23:37:38 +0200

    add dns and finger rules for draghi

--- modules/ferm/manifests/per-host.pp
+++ modules/ferm/manifests/per-host.pp
@@ -82,5 +82,17 @@ class ferm::per-host {
 		    rule            => "&SERVICE(tcp, 25)"
            }
         }
+	draghi: {
+            @ferm::rule { "dsa-bind":
+                    domain          => "(ip ip6)",
+                    description     => "Allow nameserver access",
+                    rule            => "&TCP_UDP_SERVICE(53)"
+            }
+            @ferm::rule { "dsa-finger":
+                    domain          => "(ip ip6)",
+                    description     => "Allow finger access",
+                    rule            => "&SERVICE(tcp, 79)"
+	    }
+        }
     }
 }
