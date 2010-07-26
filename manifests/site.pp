3baf652eee3d3d4c6e4091bdeb7dc577f3b235a3
Stephen Gran <steve@lobefin.net>
2010-07-26 08:22:20 +0100

    samosa gets recursor
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- manifests/site.pp
+++ manifests/site.pp
@@ -83,7 +83,7 @@ node default {
     case $hostname {
         klecker,ravel,senfl,orff: { include named::secondary }
         geo1,geo2,geo3:           { include named::geodns }
-        bartok,schein,steffani,ries,franck,liszt,master,spohr:   { include named::recursor }
+        bartok,franck,liszt,master,ries,samosa,schein,spohr,steffani:   { include named::recursor }
     }
 
     case $hostname {
