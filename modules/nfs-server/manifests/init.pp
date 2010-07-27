438ebb72ce01d9ecc6c0dd374d8e5443a6899576
Stephen Gran <steve@lobefin.net>
2010-07-27 08:59:23 +0100

    Add nfs-server module.  Still needs menu.lst modification manually for moduleless kernels. Signed-off-by: Stephen Gran <steve@lobefin.net>

---
+++ modules/nfs-server/manifests/init.pp
@@ -0,0 +1,31 @@
+class nfs-server {
+
+    include ferm::nfs-server
+
+    package {
+        nfs-common: ensure => installed;
+        nfs-kernel-server: ensure => installed;
+    }
+
+    file {
+        "/etc/default/nfs-common":
+            source  => "puppet:///nfs-server/nfs-common.default"),
+            require => Package["nfs-common"],
+            notify  => Exec["nfs-common restart"];
+        "/etc/default/nfs-kernel-server":
+            source  => "puppet:///nfs-server/nfs-kernel-server.default"),
+            require => Package["nfs-kernel-server"],
+            notify  => Exec["nfs-kernel-server restart"];
+        "/etc/modprobe.d/lockd.local":
+            source  => "puppet:///nfs-server/lockd.local.modprobe");
+    }
+
+    exec {
+        "nfs-common restart":
+            path        => "/etc/init.d:/usr/bin:/usr/sbin:/bin:/sbin",
+            refreshonly => true;
+        "nfs-kernel-server restart":
+            path        => "/etc/init.d:/usr/bin:/usr/sbin:/bin:/sbin",
+            refreshonly => true;
+    }
+}
