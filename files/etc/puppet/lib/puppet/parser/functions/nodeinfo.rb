02c7f4f4890f18696ce9e8094da69e8802282995
Peter Palfrader <peter@palfrader.org>
2010-09-03 12:10:44 +0200

    Find out if we are natted

--- files/etc/puppet/lib/puppet/parser/functions/nodeinfo.rb
+++ files/etc/puppet/lib/puppet/parser/functions/nodeinfo.rb
@@ -8,6 +8,15 @@ module Puppet::Parser::Functions
 
     results         = function_yamlinfo(host, yamlfile)
     results['ldap'] = function_ldapinfo(host, '*')
+
+    results['misc'] = {}
+    if $fqdn == host
+      # find out if we are behind nat
+      v4addrs = $v4ips.split(',')
+      intersection = v4addrs & nodeinfo['ldap']['ipHostNumber']
+      results['misc']['natted'] = intersection.empty?
+    end
+
     return(results)
   end
 end
