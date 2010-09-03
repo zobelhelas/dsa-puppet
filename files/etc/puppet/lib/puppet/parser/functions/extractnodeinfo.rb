4380075e63bb72da4d77e57b9a650cb27f670c5d
Peter Palfrader <peter@palfrader.org>
2010-09-03 14:17:18 +0200

    Sometimes this ruby puppet thing is weird

--- files/etc/puppet/lib/puppet/parser/functions/extractnodeinfo.rb
+++ files/etc/puppet/lib/puppet/parser/functions/extractnodeinfo.rb
@@ -8,10 +8,16 @@ module Puppet::Parser::Functions
 
     args.each do |key|
       raise Puppet::ParseError, "nodeinfo is not a hash at #{keys_done.join('->')}" unless ni.kind_of?(Hash)
-      return false unless ni.has_key?(key)
+      unless ni.has_key?(key)
+        ni = false
+        break
+      end
       ni = ni[key]
       keys_done << key
     end
     return ni
   end
 end
+# vim:set et:
+# vim:set sts=2 ts=2:
+# vim:set shiftwidth=2:
