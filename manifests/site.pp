98e2801edd59d7ffe82d2fe8bda10727d3079263
Peter Palfrader <peter@palfrader.org>
2011-03-02 12:24:35 +0100

    keyinfo is really more of an allnodeinfo by now

--- manifests/site.pp
+++ manifests/site.pp
@@ -17,7 +17,7 @@ node default {
     $localinfo = yamlinfo('*', "/etc/puppet/modules/debian-org/misc/local.yaml")
     $nodeinfo  = nodeinfo($fqdn, "/etc/puppet/modules/debian-org/misc/local.yaml")
     $hosterinfo = whohosts($nodeinfo, "/etc/puppet/modules/debian-org/misc/hoster.yaml")
-    $keyinfo   = allnodeinfo("sshRSAHostKey", "ipHostNumber", "purpose")
+    $allnodeinfo = allnodeinfo("sshRSAHostKey", "ipHostNumber", "purpose")
     $mxinfo    = allnodeinfo("mXRecord")
     notice("hoster for ${fqdn} is ${hosterinfo}")
 
