c02f8682d693739e457aecf1ce009df079d32a64
Peter Palfrader <peter@palfrader.org>
2011-03-02 19:47:45 +0100

    No more bind on franck

--- manifests/site.pp
+++ manifests/site.pp
@@ -98,10 +98,10 @@ node default {
     case $hostname {
         klecker,ravel,senfl,orff,draghi: { include named::authoritative }
         geo1,geo2,geo3:                  { include named::geodns }
-        franck,liszt,master,spohr,widor:   { include named::recursor }
+        liszt,master,spohr,widor:        { include named::recursor }
     }
     case $hostname {
-        ries,samosa:   {
+        franck,ries,samosa:   {
             include unbound
             $runs_local_resolver = true
         }
