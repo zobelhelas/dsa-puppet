96055f5e2bbc5c9185f1b194a039df68c9549af2
Peter Palfrader <peter@palfrader.org>
2011-03-02 12:33:16 +0100

    Revert "and merge mxinfo into allnodeinfo"
    This reverts commit d57f90694b908a8a5a326fea7597710104f9fb29.
    
    Currently we would only fetch hosts that have all of the attributes in
    question.

--- manifests/site.pp
+++ manifests/site.pp
@@ -17,7 +17,8 @@ node default {
     $localinfo = yamlinfo('*', "/etc/puppet/modules/debian-org/misc/local.yaml")
     $nodeinfo  = nodeinfo($fqdn, "/etc/puppet/modules/debian-org/misc/local.yaml")
     $hosterinfo = whohosts($nodeinfo, "/etc/puppet/modules/debian-org/misc/hoster.yaml")
-    $allnodeinfo = allnodeinfo("sshRSAHostKey", "ipHostNumber", "purpose", "mXRecord")
+    $allnodeinfo = allnodeinfo("sshRSAHostKey", "ipHostNumber", "purpose")
+    $mxinfo    = allnodeinfo("mXRecord")
     notice("hoster for ${fqdn} is ${hosterinfo}")
 
     include munin-node
