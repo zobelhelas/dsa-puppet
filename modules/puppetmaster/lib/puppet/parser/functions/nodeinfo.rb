ff71b179b92b30b65fe91e41df91c8aaa6eaa766
Peter Palfrader <peter@palfrader.org>
2011-03-02 15:46:07 +0100

    Also add v4addrs and v6addrs to nodeinfo

--- modules/puppetmaster/lib/puppet/parser/functions/nodeinfo.rb
+++ modules/puppetmaster/lib/puppet/parser/functions/nodeinfo.rb
@@ -14,10 +14,16 @@ module Puppet::Parser::Functions
     if fqdn and fqdn == host
       v4ips = lookupvar('v4ips')
       if v4ips
+        results['misc']['v4addrs'] = v4ips.split(',')
+
         # find out if we are behind nat
-        v4addrs = v4ips.split(',')
-        intersection = v4addrs & results['ldap']['ipHostNumber']
+        intersection = results['misc']['v4addrs'] & results['ldap']['ipHostNumber']
         results['misc']['natted'] = intersection.empty?
+      end
+
+      v6ips = lookupvar('v6ips')
+      if v6ips and v6ips != "no"
+        results['misc']['v6addrs'] = v6ips.split(',')
       end
     end
 
