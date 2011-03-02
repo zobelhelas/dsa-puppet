602dba70eda47b7eca3e617da57da90333eba863
Peter Palfrader <peter@palfrader.org>
2011-03-02 13:00:22 +0100

    Try to get useful error messages

--- modules/puppetmaster/lib/puppet/parser/functions/allnodeinfo.rb
+++ modules/puppetmaster/lib/puppet/parser/functions/allnodeinfo.rb
@@ -1,18 +1,22 @@
 module Puppet::Parser::Functions
   newfunction(:allnodeinfo, :type => :rvalue) do |args|
-    required = args.shift.split()
-    optional = args.shift.split()
+    begin
+      required = args.shift.split()
+      optional = args.shift.split()
 
-    allhosts = function_ldapinfo('*', required+optional )
-    res = {}
-    allhosts.each_pair do |hostname, host|
-        # If a returned value doesn't have all the attributes we're searching for, skip
-        # We'll skip if the array is empty, but we also seem to get back a nil object for empty attributes sometimes
-        next if required.any?{ |a| not host[a] or host[a].empty? }
-        res[hostname] = host
+      allhosts = function_ldapinfo('*', required+optional )
+      res = {}
+      allhosts.each_pair do |hostname, host|
+          # If a returned value doesn't have all the attributes we're searching for, skip
+          # We'll skip if the array is empty, but we also seem to get back a nil object for empty attributes sometimes
+          next if required.any?{ |a| not host[a] or host[a].empty? }
+          res[hostname] = host
+      end
+
+      return res
+    rescue => e
+      raise Puppet::ParseError, "Error in allnodeinfo: #{e.message}\n#{e.backtrace}"
     end
-
-    return res
   end
 end
 # vim:set et:
