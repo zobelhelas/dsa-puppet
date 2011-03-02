a45e9562bc9908f929b81efe532d64f20c03242a
Peter Palfrader <peter@palfrader.org>
2011-03-02 19:30:12 +0100

    we pass lists of ranges to ferm

--- modules/unbound/manifests/init.pp
+++ modules/unbound/manifests/init.pp
@@ -51,12 +51,12 @@ class unbound {
                     @ferm::rule { "dsa-dns":
                         domain          => "ip",
                         description     => "Allow nameserver access",
-                        rule            => sprintf("&TCP_UDP_SERVICE_RANGE(53, %s)", join_spc(filter_ipv4(getfromhash($nodeinfo, 'hoster', 'allow_dns_query')))),
+                        rule            => sprintf("&TCP_UDP_SERVICE_RANGE(53, (%s))", join_spc(filter_ipv4(getfromhash($nodeinfo, 'hoster', 'allow_dns_query')))),
                     }
                     @ferm::rule { "dsa-dns6":
                         domain          => "ip6",
                         description     => "Allow nameserver access",
-                        rule            => sprintf("&TCP_UDP_SERVICE_RANGE(53, %s)", join_spc(filter_ipv6(getfromhash($nodeinfo, 'hoster', 'allow_dns_query')))),
+                        rule            => sprintf("&TCP_UDP_SERVICE_RANGE(53, (%s))", join_spc(filter_ipv6(getfromhash($nodeinfo, 'hoster', 'allow_dns_query')))),
                     }
                 }
             }
