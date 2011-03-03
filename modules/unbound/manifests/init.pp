e165e5d2ac3186893d510d5391f06a78f340533d
Peter Palfrader <peter@palfrader.org>
2011-03-03 11:41:39 +0100

    I wonder if that works, II

--- modules/unbound/manifests/init.pp
+++ modules/unbound/manifests/init.pp
@@ -38,7 +38,7 @@ class unbound {
             notify  => Exec["unbound restart"],
             owner   => root,
             group   => root,
-            require => [ File[/var/lib/unbound/root.key],  File[/var/lib/unbound/debian.org.key] ],
+            require => [ File['/var/lib/unbound/root.key'],  File['/var/lib/unbound/debian.org.key'] ],
             ;
     }
 
