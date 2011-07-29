379bd17e12aeac2561c891b018ca8017a4b121ed
Peter Palfrader <peter@palfrader.org>
2011-07-29 16:23:21 +0200

    ftp-upcoming

---
+++ modules/apache2/manifests/ftp-upcoming_mirror.pp
@@ -0,0 +1,17 @@
+class apache2::ftp-upcoming_mirror inherits apache2 {
+    file {
+        "/etc/apache2/sites-available/ftp-upcoming.debian.org":
+            source  => [ "puppet:///modules/apache2/per-host/$fqdn/etc/apache2/sites-available/ftp-upcoming.debian.org",
+                         "puppet:///modules/apache2/common/etc/apache2/sites-available/ftp-upcoming.debian.org" ];
+
+    }
+
+    activate_apache_site {
+        "010-ftp-upcoming.debian.org": site => "ftp-upcoming.debian.org";
+    }
+
+}
+
+# vim:set et:
+# vim:set sts=4 ts=4:
+# vim:set shiftwidth=4:
