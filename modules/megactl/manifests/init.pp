e4d39155a3d7143b4cdc6ef6e22e0f4070235bbb
Stephen Gran <steve@lobefin.net>
2011-07-30 10:00:32 +0100

    and we want the restricted list
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/megactl/manifests/init.pp
+++ modules/megactl/manifests/init.pp
@@ -2,6 +2,11 @@ class megactl {
     package {
         megactl: ensure => installed;
     }
+    file {
+        "/etc/apt/sources.list.d/debian.restricted.list":
+            content => template("debian-org/etc/apt/sources.list.d/debian.restricted.list.erb"),
+            notify  => Exec["apt-get update"];
+    }
 }
 # vim:set et:
 # vim:set sts=4 ts=4:
