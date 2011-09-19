657d0787c147515f5c6ac67d47e719db9b167532
Peter Palfrader <peter@palfrader.org>
2011-09-19 08:45:42 +0200

    add www.backports.org redirect on all backports hosts

--- modules/apache2/manifests/backports_mirror.pp
+++ modules/apache2/manifests/backports_mirror.pp
@@ -3,11 +3,15 @@ class apache2::backports_mirror inherits apache2 {
         "/etc/apache2/sites-available/backports.debian.org":
             source  => [ "puppet:///modules/apache2/per-host/$fqdn/etc/apache2/sites-available/backports.debian.org",
                          "puppet:///modules/apache2/common/etc/apache2/sites-available/backports.debian.org" ];
+        "/etc/apache2/sites-available/www.backports.org":
+            source  => [ "puppet:///modules/apache2/per-host/$fqdn/etc/apache2/sites-available/www.backports.org",
+                         "puppet:///modules/apache2/common/etc/apache2/sites-available/www.backports.org" ];
 
     }
 
     activate_apache_site {
         "010-backports.debian.org": site => "backports.debian.org";
+        "010-www.backports.org": site => "www.backports.org";
     }
 
     enable_module {
