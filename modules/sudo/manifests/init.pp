3482abc30588447604d3da5d70b18b50eb77676f
Peter Palfrader <peter@palfrader.org>
2011-11-05 18:06:14 +0100

    whitespace change

--- modules/sudo/manifests/init.pp
+++ modules/sudo/manifests/init.pp
@@ -1,20 +1,20 @@
 class sudo {
-	package { sudo: ensure => installed }
+    package { sudo: ensure => installed }
 
-	file { "/etc/sudoers":
-		owner   => root,
-		group   => root,
-		mode    => 440,
-		content => template("sudo/sudoers.erb"),
-		require => Package["sudo"]
+    file { "/etc/sudoers":
+        owner   => root,
+        group   => root,
+        mode    => 440,
+        content => template("sudo/sudoers.erb"),
+        require => Package["sudo"]
                 ;
-	       "/etc/pam.d/sudo":
-		source  => [ "puppet:///modules/sudo/per-host/$fqdn/pam",
-		             "puppet:///modules/sudo/common/pam" ],
-		require => Package["sudo"]
+           "/etc/pam.d/sudo":
+        source  => [ "puppet:///modules/sudo/per-host/$fqdn/pam",
+                     "puppet:///modules/sudo/common/pam" ],
+        require => Package["sudo"]
                 ;
 
-	}
+    }
 }
 # vim:set et:
 # vim:set sts=4 ts=4:
