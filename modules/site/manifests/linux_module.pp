5c3ced682c222d6dca1882b1c07eefc08f2d249b
Stephen Gran <steve@lobefin.net>
2012-04-05 15:00:19 +0100

    guard against accidentally doing something
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/site/manifests/linux_module.pp
+++ modules/site/manifests/linux_module.pp
@@ -1,19 +1,21 @@
 define site::linux_module ($ensure = present) {
-	case $ensure {
-		present: {
-			exec { "append_module_${name}":
-				command => "echo '${name}' >> /etc/modules",
-				unless => "grep -q -F -x '${name}' /etc/modules",
+	if $::kernel == linux {
+		case $ensure {
+			present: {
+				exec { "append_module_${name}":
+					command => "echo '${name}' >> /etc/modules",
+					unless => "grep -q -F -x '${name}' /etc/modules",
+				}
 			}
-		}
-		absent: {
-			exec { "remove_module_${name}":
-				command => "sed -i -e'/^${name}\$/d' /etc/modules",
-				onlyif => "grep -q -F -x '${name}' /etc/modules",
+			absent: {
+				exec { "remove_module_${name}":
+					command => "sed -i -e'/^${name}\$/d' /etc/modules",
+					onlyif => "grep -q -F -x '${name}' /etc/modules",
+				}
 			}
-		}
-		default: {
-			err("invalid ensure value ${ensure}")
+			default: {
+				err("invalid ensure value ${ensure}")
+			}
 		}
 	}
 }
