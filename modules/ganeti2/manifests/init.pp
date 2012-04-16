d7968da127e174498bf1e122bb879f3e87c590ba
Luca Filipozzi <lfilipoz@emyr.net>
2012-04-16 09:15:56 +0000

    add more packages that the ganeti module should install

--- modules/ganeti2/manifests/init.pp
+++ modules/ganeti2/manifests/init.pp
@@ -8,6 +8,14 @@ class ganeti2 {
 		ensure => installed
 	}
 
+	package { 'ganeti-instance-debootstrap':
+		ensure => installed
+	}
+
+	package { 'ganeti-htools':
+		ensure => installed
+	}
+
 	@ferm::rule { 'dsa-ganeti-v4':
 		description => 'Allow ganeti from ganeti master',
 		rule        => 'proto tcp mod state state (NEW) dport (1811) @subchain \'ganeti\' { saddr ($HOST_GANETI_V4) ACCEPT; }',
