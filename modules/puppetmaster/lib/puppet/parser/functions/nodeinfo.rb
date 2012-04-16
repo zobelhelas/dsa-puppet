dec805815e06f1a36e55ee63b2a64c161d91f046
Stephen Gran <steve@lobefin.net>
2012-04-16 19:56:21 +0100

    see if we can't get buildd info from ldap
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/puppetmaster/lib/puppet/parser/functions/nodeinfo.rb
+++ modules/puppetmaster/lib/puppet/parser/functions/nodeinfo.rb
@@ -13,6 +13,7 @@ module Puppet::Parser::Functions
         raise Puppet::ParseError, "Host #{host} does not have ipHostNumber values in ldap"
       end
       nodeinfo['hoster'] = function_whohosts(nodeinfo['ldap']['ipHostNumber'], "/etc/puppet/modules/debian-org/misc/hoster.yaml")
+      nodeinfo['buildd'] = nodeinfo['ldap']['purpose'].include?('buildd')
 
       nodeinfo['misc'] = {}
       fqdn = lookupvar('::fqdn')
