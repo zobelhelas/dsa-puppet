b4ac9308608e43cf12b4609b57f8dee9b254e049
Stephen Gran <steve@lobefin.net>
2012-04-16 20:07:10 +0100

    more printf debug
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/site/manifests/init.pp
+++ modules/site/manifests/init.pp
@@ -4,8 +4,7 @@ class site {
 	$nodeinfo  = nodeinfo($::fqdn, '/etc/puppet/modules/debian-org/misc/local.yaml')
 	$allnodeinfo = allnodeinfo('sshRSAHostKey ipHostNumber', 'purpose mXRecord physicalHost purpose')
 	notice( sprintf('hoster for %s is %s', $::fqdn, getfromhash($nodeinfo, 'hoster', 'name') ) )
-	notice( sprintf('facter hoster for %s is %s', $::fqdn, $::hoster) )
-	notice( sprintf('hiera resolver for %s is %s', $::fqdn, join_spc(hiera('nameservers') ) ) )
+	notice( sprintf('buildd status for %s is %s', $::fqdn, getfromhash($nodeinfo, 'buildd') ) )
 
 	service { 'procps':
 		hasstatus   => false,
