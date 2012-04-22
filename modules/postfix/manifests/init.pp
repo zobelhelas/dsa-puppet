faa394e092aa7af4c280508a215aff53ceb1db05
Stephen Gran <steve@lobefin.net>
2012-04-22 20:00:13 +0100

    oops, need a name
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/postfix/manifests/init.pp
+++ modules/postfix/manifests/init.pp
@@ -9,7 +9,7 @@ class postfix {
 	munin::check { 'ps_smtp': script => 'ps_' }
 	munin::check { 'ps_smtpd': script => 'ps_' }
 
-	@ferm::rule {
+	@ferm::rule { 'smtp':
 		domain      => '(ip ip6)',
 		description => 'Allow smtp access',
 		rule        => '&SERVICE(tcp, 25)'
