001757dbe641c691acea373e5579a8f7f2e47fe5
Stephen Gran <steve@lobefin.net>
2012-04-26 09:02:46 +0100

    keep transitioning to roles
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- manifests/site.pp
+++ manifests/site.pp
@@ -33,6 +33,7 @@ node default {
 	include hardware
 	include nagios::client
 	include resolv
+	include roles
 
 	if $::hostname in [pasquini,tristano] {
 		include ganeti2
@@ -60,54 +61,6 @@ node default {
 
 	if $::lsbdistcodename != 'lenny' {
 		include unbound
-	}
-
-	if getfromhash($site::nodeinfo, 'puppetmaster') {
-		include puppetmaster
-	}
-
-	if getfromhash($site::nodeinfo, 'muninmaster') {
-		include munin::master
-	}
-
-	if getfromhash($site::nodeinfo, 'nagiosmaster') {
-		include nagios::server
-	}
-
-	if getfromhash($site::nodeinfo, 'buildd') {
-		include buildd
-	}
-
-	if $::hostname in [chopin,franck,morricone,bizet] {
-		include roles::dakmaster
-	}
-
-	if getfromhash($site::nodeinfo, 'apache2_security_mirror') {
-		include roles::security_mirror
-	}
-
-	if getfromhash($site::nodeinfo, 'apache2_www_mirror') {
-		include roles::www_mirror
-	}
-
-	if getfromhash($site::nodeinfo, 'apache2_backports_mirror') {
-		include roles::backports_mirror
-	}
-
-	if getfromhash($site::nodeinfo, 'ftp.d.o') {
-		include roles::ftp
-	}
-
-	if getfromhash($site::nodeinfo, 'ftp.upload.d.o') {
-		include roles::ftp_upload
-	}
-
-	if $::hostname in [bizet,morricone] {
-		include roles::backports_master
-	}
-
-	if getfromhash($site::nodeinfo, 'apache2_ftp-upcoming_mirror') {
-		include roles::ftp-upcoming_mirror
 	}
 
 	if $::apache2 {
