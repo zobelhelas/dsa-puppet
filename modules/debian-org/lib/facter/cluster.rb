3c0b065a5fef2f3b734336d1aa1a7d4d41cb4b97
Luca Filipozzi <lfilipoz@emyr.net>
2012-05-16 20:14:41 +0000

    refacter facter code

--- modules/debian-org/lib/facter/cluster.rb
+++ modules/debian-org/lib/facter/cluster.rb
@@ -1,17 +1,11 @@
-Facter.add('cluster') do
-  setcode do
-    if FileTest.exist?('/usr/sbin/gnt-cluster')
-      if system('/usr/sbin/gnt-cluster getmaster >/dev/null')
-        require 'json'
-        config = '/var/lib/ganeti/config.data'
-        if FileTest.exist?(config)
-          JSON.parse(File.read(config))['cluster']['cluster_name']
-        else
-          ''
-        end
+if FileTest.exist?('/usr/sbin/gnt-cluster')
+  if system('/usr/sbin/gnt-cluster getmaster >/dev/null')
+    require 'json'
+    config = '/var/lib/ganeti/config.data'
+    Facter.add('cluster') do
+      setcode do
+        JSON.parse(File.read(config))['cluster']['cluster_name']
       end
-    else
-      ''
     end
   end
 end
