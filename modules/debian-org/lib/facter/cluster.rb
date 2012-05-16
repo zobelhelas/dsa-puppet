5f7c97946997b63e32b009b11578159e826229ad
Luca Filipozzi <lfilipoz@emyr.net>
2012-05-16 17:55:55 +0000

    add fact for ganeti cluster

---
+++ modules/debian-org/lib/facter/cluster.rb
@@ -0,0 +1,12 @@
+Facter.add('cluster') do
+  setcode do
+    if system('/usr/sbin/gnt-cluster getmaster')
+      require 'json'
+      config = '/var/lib/ganeti/config.data'
+      if FileTest.exist?(config)
+        JSON.parse(File.read(config))['cluster']['cluster_name']
+      else
+        ''
+      end
+  end
+end
