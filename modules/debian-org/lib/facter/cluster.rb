83940f5dadd68d50afe4191c70ac882f8e676115
Luca Filipozzi <lfilipoz@emyr.net>
2012-05-16 18:22:12 +0000

    frustrating development cycle

--- modules/debian-org/lib/facter/cluster.rb
+++ modules/debian-org/lib/facter/cluster.rb
@@ -1,13 +1,17 @@
 Facter.add('cluster') do
   setcode do
-    if system('/usr/sbin/gnt-cluster getmaster >/dev/null')
-      require 'json'
-      config = '/var/lib/ganeti/config.data'
-      if FileTest.exist?(config)
-        JSON.parse(File.read(config))['cluster']['cluster_name']
-      else
-        ''
+    if FileTest.exist?('/usr/sbin/gnt-cluster')
+      if system('/usr/sbin/gnt-cluster getmaster >/dev/null')
+        require 'json'
+        config = '/var/lib/ganeti/config.data'
+        if FileTest.exist?(config)
+          JSON.parse(File.read(config))['cluster']['cluster_name']
+        else
+          ''
+        end
       end
+    else
+      ''
     end
   end
 end
