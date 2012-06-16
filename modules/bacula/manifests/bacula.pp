18a560e525e4027fb7d59caac5ee59b66e158ea0
Tollef Fog Heen <tfheen@err.no>
2012-06-16 10:12:59 +0200

    Move storage to beethoven

--- modules/bacula/manifests/bacula.pp
+++ modules/bacula/manifests/bacula.pp
@@ -12,7 +12,7 @@ class bacula {
 
   $bacula_director_address  = "dinis.debian.org"
   $bacula_director_port     = 9101
-  $bacula_storage_address   = "jommeli.debian.org"
+  $bacula_storage_address   = "beethoven.debian.org"
   $bacula_storage_port      = 9103
   $bacula_client_port       = 9102
 
