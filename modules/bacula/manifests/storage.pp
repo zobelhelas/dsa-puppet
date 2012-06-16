ad0146116b09345295d16060416a48f80ee68ebe
Tollef Fog Heen <tfheen@err.no>
2012-06-16 17:10:52 +0200

    Whitespace, cargo-cult a ferm rule bit

--- modules/bacula/manifests/storage.pp
+++ modules/bacula/manifests/storage.pp
@@ -29,13 +29,14 @@ class bacula::storage inherits bacula {
   @ferm::rule { 'dsa-bacula-sd-v4':
     domain      => '(ip)',
     description => 'Allow bacula-sd access from director and clients',
-
-    rule            => 'proto tcp mod state state (NEW) dport (bacula-sd) @subchain \'bacula-sd\' { saddr ($HOST_DEBIAN_V4) ACCEPT; }',
+    rule         => 'proto tcp mod state state (NEW) dport (bacula-sd) @subchain \'bacula-sd\' { saddr ($HOST_DEBIAN_V4) ACCEPT; }',
+    notarule        => true,
   }
+
   @ferm::rule { 'dsa-bacula-sd-v6':
     domain      => '(ip6)',
     description => 'Allow bacula-sd access from director and clients',
-
-    rule            => 'proto tcp mod state state (NEW) dport (bacula-sd) @subchain \'bacula-sd\' { saddr ($HOST_DEBIAN_V6) ACCEPT; }',
-
+    rule         => 'proto tcp mod state state (NEW) dport (bacula-sd) @subchain \'bacula-sd\' { saddr ($HOST_DEBIAN_V6) ACCEPT; }',
+    notarule        => true,
+  }
 }
