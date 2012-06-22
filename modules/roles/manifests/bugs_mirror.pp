7213fa6c65577890add1165f3e7c0eb190f39ecb
Peter Palfrader <peter@palfrader.org>
2012-06-22 20:32:19 +0200

    http://bugs-mirror.d.o should not work

--- modules/roles/manifests/bugs_mirror.pp
+++ modules/roles/manifests/bugs_mirror.pp
@@ -4,4 +4,12 @@ class roles::bugs_mirror {
 		source      => 'puppet:///modules/roles/bugs_mirror/rsyncd.conf',
 		max_clients => 100,
 	}
+
+	if $::apache2 {
+		apache2::site { '009-bugs-mirror.debian.org':
+			site   => 'bugs-mirror.debian.org',
+			source => 'puppet:///modules/roles/bugs_mirror/bugs-mirror.debian.org',
+		}
+	}
+
 }
