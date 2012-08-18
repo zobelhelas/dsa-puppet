ebaec472c60735e1f855d3893100aa4d67fce817
Peter Palfrader <peter@palfrader.org>
2012-08-18 21:25:26 +0200

    pg on v6

--- modules/ferm/manifests/per-host.pp
+++ modules/ferm/manifests/per-host.pp
@@ -47,6 +47,12 @@ class ferm::per-host {
 				description     => 'Allow postgress access',
 				rule            => '&SERVICE_RANGE(tcp, 5433, ( 206.12.19.0/24 ))'
 			}
+			@ferm::rule { 'dsa-postgres-danzi6':
+				domain          => 'ip6',
+				description     => 'Allow postgress access',
+				rule            => '&SERVICE_RANGE(tcp, 5433, ( 2607:f8f0:610:4000::/64 ))'
+			}
+
 			@ferm::rule { 'dsa-postgres2-danzi':
 				description     => 'Allow postgress access2',
 				rule            => '&SERVICE_RANGE(tcp, 5437, ( 206.12.19.0/24 ))'
