5275770968b17ca00ae43f19c92ffa825cda6f71
Stephen Gran <steve@lobefin.net>
2012-10-07 15:58:13 +0100

    add wheezy support
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/motd/manifests/init.pp
+++ modules/motd/manifests/init.pp
@@ -8,17 +8,34 @@
 #
 class motd {
 
-	file { '/etc/motd.tail':
-		notify  => Exec['updatemotd'],
-		content => template('motd/motd.erb')
-	}
-	file { '/etc/motd':
-		ensure => link,
-		target => '/var/run/motd'
+	if $::lsbdistcodename == 'wheezy' {
+		$fname  = '/etc/update-motd.d/puppet-motd'
+		$notify = undef
+		$mode   = '0555'
+
+		file { '/etc/motd':
+			ensure  => present,
+			replace => false
+		}
+
+	} elsif $::lsbdistcodename == 'squeeze' {
+		$fname  = '/etc/motd.tail'
+		$notify = Exec['updatemotd']
+		$mode   = '0444'
+
+		file { '/etc/motd':
+			ensure => link,
+			target => '/var/run/motd'
+		}
+		exec { 'updatemotd':
+			command     => 'uname -snrvm > /var/run/motd && cat /etc/motd.tail >> /var/run/motd',
+			refreshonly => true,
+		}
 	}
 
-	exec { 'updatemotd':
-		command     => 'uname -snrvm > /var/run/motd && cat /etc/motd.tail >> /var/run/motd',
-		refreshonly => true,
+	file { $fname:
+		notify  => $notify,
+		mode    => $mode,
+		content => template('motd/motd.erb')
 	}
 }
