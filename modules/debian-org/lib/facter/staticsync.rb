33d6937e49a8ef0990c6eb92eda31dd93efcd5dd
Peter Palfrader <peter@palfrader.org>
2012-12-08 14:46:43 +0100

    Facter for staticsync's ssh key

---
+++ modules/debian-org/lib/facter/staticsync.rb
@@ -0,0 +1,32 @@
+begin
+    require 'etc'
+
+    Facter.add("staticsync_key") do
+        setcode do
+            key = nil
+            keyfile = '/home/staticsync/.ssh/id_rsa.pub'
+            if FileTest.exist?(keyfile)
+                key = File.open(keyfile).read.chomp
+            end
+            key
+        end
+    end
+
+    Facter.add("staticsync_user_exists") do
+        setcode do
+            result = ''
+            begin
+                if Etc.getpwnam('staticsync')
+                    result = true
+                end
+            rescue ArgumentError
+            end
+            result
+        end
+    end
+
+rescue Exception => e
+end
+# vim:set et:
+# vim:set ts=4:
+# vim:set shiftwidth=4:
