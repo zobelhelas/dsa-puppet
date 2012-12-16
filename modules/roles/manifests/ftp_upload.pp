b0dd1aa9d67bc92d097c1ad23f42bbedd173b756
Stephen Gran <steve@lobefin.net>
2012-12-16 10:09:35 +0000

    bye bye kassia
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/roles/manifests/ftp_upload.pp
+++ modules/roles/manifests/ftp_upload.pp
@@ -1,21 +1,10 @@
 class roles::ftp_upload {
 
-	$bind = $::hostname ? {
-		kassia  => '::ffff:130.89.149.224',
-		default => '',
-	}
-
-	$bind6 = $::hostname ? {
-		kassia  => '2001:610:1908:a000:21b:78ff:fe75:3d46',
-		default => '',
-	}
-
 	vsftpd::site { 'ftp-upload':
 		banner     => 'ftp.upload.debian.org FTP server',
 		logfile    => '/var/log/ftp/vsftpd-ftp.upload.debian.org.log',
 		writable   => true,
 		chown_user => dak,
-		bind       => $bind,
 		root       => '/srv/upload.debian.org/ftp',
 	}
 
@@ -25,7 +14,6 @@ class roles::ftp_upload {
 			logfile    => '/var/log/ftp/vsftpd-ftp.upload.debian.org.log',
 			writable   => true,
 			chown_user => dak,
-			bind       => $bind6,
 			root       => '/srv/upload.debian.org/ftp',
 		}
 	}
