0f364e0ba944a33544fa15aef37d584a49320df1
Peter Palfrader <peter@palfrader.org>
2013-04-13 22:51:55 +0200

    systemproductname facter wants dmidecode

--- modules/debian-org/lib/facter/system-hw.rb
+++ modules/debian-org/lib/facter/system-hw.rb
@@ -1,6 +1,10 @@
 Facter.add("systemproductname") do
 	confine :kernel => :linux
 	setcode do
-		%x{dmidecode -s system-product-name}.chomp.strip
+		if FileTest.exist?("/usr/sbin/dmidecode")
+			%x{/usr/sbin/dmidecode -s system-product-name}.chomp.strip
+		else
+			''
+		end
 	end
 end
