d6b9140b5ddd8ecfe1f0235dfb2d4244ad773551
Peter Palfrader <peter@palfrader.org>
2013-04-13 11:59:05 +0200

    and strip

--- modules/debian-org/lib/facter/system-hw.rb
+++ modules/debian-org/lib/facter/system-hw.rb
@@ -1,6 +1,6 @@
 Facter.add("systemproductname") do
 	confine :kernel => :linux
 	setcode do
-		%x{dmidecode -s system-product-name}.chomp
+		%x{dmidecode -s system-product-name}.chomp.strip
 	end
 end
