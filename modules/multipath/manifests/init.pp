ab2ed88279650106f252798cf1c6ba9b5ddc005b
Peter Palfrader <peter@palfrader.org>
2013-04-14 00:07:14 +0200

    multipath config

---
+++ modules/multipath/manifests/init.pp
@@ -0,0 +1,27 @@
+class multipath {
+	case $::hostname {
+		bm-bl1,bm-bl2,bm-bl3,bm-bl4,bm-bl5,bm-bl6,bm-bl7,bm-bl8,bm-bl9,bm-bl10,bm-bl11,bm-bl12,bm-bl13,bm-bl14: {
+			$conffile = 'bm-multipath.conf';
+		}
+		default: {
+			$conffile = ''
+		}
+	}
+
+	if $conffile != '' {
+		package { 'multipath-tools':
+			ensure => installed,
+		}
+		exec { 'multipath reload':
+			path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+			command     => 'service multipath-tools reload',
+			refreshonly => true,
+			require =>  Package['multipath-tools'],
+		}
+
+		file { '/etc/multipath.conf':
+			source  => "puppet:///modules/multipath/$conffile",
+			notify  => Exec['multipath reload']
+		}
+	}
+}
