a5ae94cf7f5acea64f03166625bc7d5b7d18b080
Peter Palfrader <peter@palfrader.org>
2013-04-21 20:49:37 +0200

    fix mode

--- modules/porterbox/manifests/init.pp
+++ modules/porterbox/manifests/init.pp
@@ -7,20 +7,23 @@ class porterbox {
 	file { '/etc/schroot/dsa/config':
 		source  => 'puppet:///modules/porterbox/schroot-dsa/config',
 	}
+	file { '/etc/schroot/dsa/default-mirror':
+		content => template('porterbox/default-mirror.erb'),
+	}
 	file { '/etc/schroot/setup.d/99porterbox-extra-apt-options':
+		mode    => 555,
 		source  => 'puppet:///modules/porterbox/schroot-setup.d/99porterbox-extra-apt-options',
 	}
 	file { '/etc/schroot/setup.d/99porterbox-extra-sources':
+		mode    => 555,
 		source  => 'puppet:///modules/porterbox/schroot-setup.d/99porterbox-extra-sources',
 	}
 	file { '/usr/local/bin/dd-schroot-cmd':
+		mode    => 555,
 		source  => 'puppet:///modules/porterbox/dd-schroot-cmd',
 	}
 	file { '/usr/local/sbin/setup-dchroot':
+		mode    => 555,
 		source  => 'puppet:///modules/porterbox/setup-dchroot',
-	}
-
-	file { '/etc/schroot/dsa/default-mirror':
-		content => template('porterbox/default-mirror.erb'),
 	}
 }
