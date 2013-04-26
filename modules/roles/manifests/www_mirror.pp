0917b97c378fe627879ec65d7beea2643a73f2f0
Peter Palfrader <peter@palfrader.org>
2013-04-26 11:32:29 +0200

    Use the template

--- modules/roles/manifests/www_mirror.pp
+++ modules/roles/manifests/www_mirror.pp
@@ -4,7 +4,6 @@ class roles::www_mirror {
 
 	apache2::site { '010-www.debian.org':
 		site   => 'www.debian.org',
-		source => 'puppet:///modules/roles/www_mirror/www.debian.org',
 		content => template('roles/apache-www.debian.org.erb'),
 	}
 }
