49223ad1e8bf7a056e780783c3155d9099f88352
Peter Palfrader <peter@palfrader.org>
2013-04-26 11:36:37 +0200

    Use vhost_listen

--- modules/roles/manifests/www_mirror.pp
+++ modules/roles/manifests/www_mirror.pp
@@ -2,6 +2,7 @@ class roles::www_mirror {
 	# see also static mirrors.
 
 	$wwwdo_document_root = '/srv/www.debian.org/www'
+	$vhost_listen = '*:80'
 	apache2::site { '010-www.debian.org':
 		site   => 'www.debian.org',
 		content => template('roles/apache-www.debian.org.erb'),
