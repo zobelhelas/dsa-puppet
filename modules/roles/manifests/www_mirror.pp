bc12cd8473771b6feb8de7d688a783b519311d44
Peter Palfrader <peter@palfrader.org>
2013-04-26 11:27:17 +0200

    Make the www.d.o apache config a template

--- modules/roles/manifests/www_mirror.pp
+++ modules/roles/manifests/www_mirror.pp
@@ -1,7 +1,10 @@
 class roles::www_mirror {
 
+	$wwwdo_document_root = '/srv/www.debian.org/www'
+
 	apache2::site { '010-www.debian.org':
 		site   => 'www.debian.org',
 		source => 'puppet:///modules/roles/www_mirror/www.debian.org',
+		content => template('roles/apache-www.debian.org.erb'),
 	}
 }
