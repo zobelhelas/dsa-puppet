8368310b0e5a5193c5703404f1390fd5c18066ff
Peter Palfrader <peter@palfrader.org>
2013-04-26 11:34:16 +0200

    Have www on the static mirrors

--- modules/roles/manifests/www_mirror.pp
+++ modules/roles/manifests/www_mirror.pp
@@ -1,7 +1,7 @@
 class roles::www_mirror {
+	# see also static mirrors.
 
 	$wwwdo_document_root = '/srv/www.debian.org/www'
-
 	apache2::site { '010-www.debian.org':
 		site   => 'www.debian.org',
 		content => template('roles/apache-www.debian.org.erb'),
