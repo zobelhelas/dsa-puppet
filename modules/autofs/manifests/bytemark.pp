0e4d2cbe94f4aeeb43933ca02ef5841a33d5a8ec
Peter Palfrader <peter@palfrader.org>
2013-05-12 18:33:17 +0200

    mirror symlinks

--- modules/autofs/manifests/bytemark.pp
+++ modules/autofs/manifests/bytemark.pp
@@ -21,4 +21,9 @@ class autofs::bytemark {
 		source  => "puppet:///modules/autofs/bytemark/auto.dsa",
 		notify  => Exec['autofs reload']
 	}
+
+	file { '/srv/mirrors': ensure  => directory }
+	file { '/srv/mirrors/debian': ensure  => '/auto.dsa/debian' }
+	file { '/srv/mirrors/debian-backports': ensure  => '/auto.dsa/debian-backports' }
+	file { '/srv/mirrors/debian-security': ensure  => '/auto.dsa/debian-security' }
 }
