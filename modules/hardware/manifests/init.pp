a7a022f3a3b65297ebed071701053b0d70e05484
Peter Palfrader <peter@palfrader.org>
2013-05-19 12:14:44 +0200

    old hp-health for dl385g1

--- modules/hardware/manifests/init.pp
+++ modules/hardware/manifests/init.pp
@@ -1,11 +1,9 @@
 class hardware {
 	include hardware::raid
 
-	case $::systemproductname {
-		"ProLiant BL495c G5": {
-			file { '/etc/apt/preferences.d/dsa-hp-tools':
-				content => "Package: hp-health\nPin: version 8.6*\nPin-Priority: 1100\n"
-			}
+	if $::systemproductname in ["ProLiant BL495c G5", "ProLiant DL385 G1"] {
+		file { '/etc/apt/preferences.d/dsa-hp-tools':
+			content => "Package: hp-health\nPin: version 8.6*\nPin-Priority: 1100\n"
 		}
 	}
 }
