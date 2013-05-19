b135564a1dfab78b25561ed250eda08823ca9e42
Peter Palfrader <peter@palfrader.org>
2013-05-19 19:31:08 +0200

    there is no more php5-suhosin

--- modules/apache2/manifests/init.pp
+++ modules/apache2/manifests/init.pp
@@ -68,19 +68,6 @@ class apache2 {
 		script => 'ps_',
 	}
 
-	if $php5 {
-		package { 'php5-suhosin':
-			ensure  => installed,
-			require => Package['apache2'],
-		}
-
-		file { '/etc/php5/conf.d/suhosin.ini':
-			source  => 'puppet:///modules/apache2/suhosin.ini',
-			require => Package['php5-suhosin'],
-			notify  => Service['apache2'],
-		}
-	}
-
 	if $::hostname in [holter,beach,buxtehude,picconi,pkgmirror-1and1] {
 		include apache2::dynamic
 	} else {
