f7d9c31205ea2231f0fa60072f4e90800455c5c9
Peter Palfrader <peter@palfrader.org>
2013-05-25 13:33:56 +0200

    actually enable module

--- manifests/site.pp
+++ manifests/site.pp
@@ -37,6 +37,7 @@ node default {
 	include unbound
 	include bacula::client
 	include autofs
+	include lvm
 
 	if $::hostname in [pasquini,tristano,bertali,boito,rossini,dijkstra,luchesi,byrd,clementi,czerny,bm-bl1,bm-bl2,bm-bl3,bm-bl4,bm-bl5,bm-bl6,bm-bl7,bm-bl8,bm-bl9,bm-bl10,bm-bl11,bm-bl12,bm-bl13,bm-bl14] {
 		include ganeti2
