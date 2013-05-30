c32bd441c25d9291af7bd5f1d74be0444382cba9
Peter Palfrader <peter@palfrader.org>
2013-05-30 11:51:17 +0200

    on freebsd, install our procpc properly; also set accept_ra to 0

--- modules/kfreebsd/manifests/init.pp
+++ modules/kfreebsd/manifests/init.pp
@@ -13,11 +13,22 @@ class kfreebsd {
 		source => 'puppet:///modules/kfreebsd/procps.init',
 		mode   => '0555',
 		before => Service['procps'],
+		notify  => Exec['update-rc.d procps defaults']
 	}
 
 	site::sysctl { 'maxfiles':
 		key    => 'kern.maxfiles',
 		value  => 65536,
 		target => 'GNU/kFreeBSD',
+	}
+	site::sysctl { 'accept_ra':
+		key    => 'net.inet6.ip6.accept_rtadv',
+		value  => 0,
+		target => 'GNU/kFreeBSD',
+	}
+
+	exec { 'update-rc.d procps defaults':
+		path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+		refreshonly => true,
 	}
 }
