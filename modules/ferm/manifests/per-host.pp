d5728bdb4fd44fc50442347a977568a2d93287ea
Tollef Fog Heen <tfheen@err.no>
2013-06-16 01:07:37 +0200

    Drop piatti

--- modules/ferm/manifests/per-host.pp
+++ modules/ferm/manifests/per-host.pp
@@ -12,7 +12,7 @@ class ferm::per-host {
 	}
 
 	case $::hostname {
-		piatti,samosa: {
+		samosa: {
 			@ferm::rule { 'dsa-udd-stunnel':
 				description  => 'port 8080 for udd stunnel',
 				rule         => '&SERVICE_RANGE(tcp, http-alt, ( 192.25.206.16 70.103.162.29 217.196.43.134 ))'
