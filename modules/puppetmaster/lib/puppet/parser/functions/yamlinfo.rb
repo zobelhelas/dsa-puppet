c2d68a937252561e125c130131697de528638b70
Peter Palfrader <peter@palfrader.org>
2013-06-20 11:03:18 +0200

    Move reservedaddrs directly into exim.conf

--- modules/puppetmaster/lib/puppet/parser/functions/yamlinfo.rb
+++ modules/puppetmaster/lib/puppet/parser/functions/yamlinfo.rb
@@ -28,8 +28,7 @@ module Puppet::Parser::Functions
       results['smarthost']      = ''
       results['heavy_exim']     = ''
       results['smarthost_port'] = 587
-      results['reservedaddrs']  = '0.0.0.0/8 : 127.0.0.0/8 : 10.0.0.0/8 : 169.254.0.0/16 : 172.16.0.0/12 : 192.0.0.0/17 : 192.168.0.0/16 : 224.0.0.0/4 : 240.0.0.0/5 : 248.0.0.0/5'
-    
+
       if yaml['host_settings'].kind_of?(Hash)
         yaml['host_settings'].each_pair do |property, values|
           if values.kind_of?(Hash)
