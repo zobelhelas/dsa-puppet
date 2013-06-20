6c1ddb4d3292d02659f13442e82a89a63ab006ae
Peter Palfrader <peter@palfrader.org>
2013-06-20 13:17:37 +0200

    auto-create and update porter chroots

--- modules/porterbox/manifests/init.pp
+++ modules/porterbox/manifests/init.pp
@@ -33,4 +33,11 @@ class porterbox {
 		mode    => 555,
 		source  => 'puppet:///modules/porterbox/setup-dchroot',
 	}
+	file { '/usr/local/sbin/setup-all-dchroots':
+		mode    => 555,
+		source  => 'puppet:///modules/porterbox/setup-all-dchroots',
+	}
+	file { '/etc/cron.d/puppet-update-dchroots':
+		content  => '0 3 * * 0 root PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/sbin:/usr/local/bin chronic setup-all-dchroots',
+	}
 }
