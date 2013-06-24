533f6dfc2e81092416791506c4f89ff9ad028910
Peter Palfrader <peter@palfrader.org>
2013-06-24 21:13:25 +0200

    Try to bind a different IP address

--- modules/roles/manifests/syncproxy.pp
+++ modules/roles/manifests/syncproxy.pp
@@ -1,6 +1,10 @@
 class roles::syncproxy {
 	rsync::site { 'syncproxy':
 		source => 'puppet:///modules/roles/syncproxy/rsyncd.conf',
+		bind   => $::hostname ? {
+			'milanollo' => 5.153.231.9,
+			default => ''
+		}
 	}
 
 	file { '/etc/rsyncd':
