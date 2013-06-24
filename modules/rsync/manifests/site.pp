aae8b555c902a217d68dc96ccd50ae6144e8e686
Peter Palfrader <peter@palfrader.org>
2013-06-24 21:23:09 +0200

    Try to handle xinetd flags sanely

--- modules/rsync/manifests/site.pp
+++ modules/rsync/manifests/site.pp
@@ -1,5 +1,6 @@
 define rsync::site (
 	$bind='',
+	$bind6='',
 	$source='',
 	$content='',
 	$fname='',
@@ -39,6 +40,7 @@ define rsync::site (
 
 	xinetd::service { "rsync-${name}":
 		bind        => $bind,
+		bind6       => $bind6,
 		id          => "${name}-rsync",
 		server      => '/usr/bin/rsync',
 		port        => 'rsync',
