4249905ee53b7dec9600d129a97611002c1699da
Peter Palfrader <peter@palfrader.org>
2013-09-16 09:03:50 +0200

    buildd ssl cert

--- modules/roles/manifests/buildd_master.pp
+++ modules/roles/manifests/buildd_master.pp
@@ -5,4 +5,7 @@ class roles::buildd_master {
 		site   => 'buildd.debian.org',
 		source => 'puppet:///modules/roles/buildd_master/apache.conf'
 	}
+	ssl::service { 'buildd.debian.org':
+		notify => Service['apache2'],
+	}
 }
