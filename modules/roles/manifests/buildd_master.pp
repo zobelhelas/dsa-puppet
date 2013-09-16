3e7022d4137ec1f484e1106fb8cca130af45540b
Stephen Gran <steve@lobefin.net>
2013-09-16 08:00:56 +0100

    add mod macro
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/roles/manifests/buildd_master.pp
+++ modules/roles/manifests/buildd_master.pp
@@ -1,4 +1,6 @@
 class roles::buildd_master {
+	include apache2::macro
+
 	apache2::site { '010-buildd.debian.org':
 		site   => 'buildd.debian.org',
 		source => 'puppet:///modules/roles/buildd_master/apache.conf'
