6ee2a09e76abc7d9676394954efbaa9ef276e986
Peter Palfrader <peter@palfrader.org>
2013-12-15 14:40:29 +0100

    Add script to mail on large homedirs

--- modules/porterbox/manifests/init.pp
+++ modules/porterbox/manifests/init.pp
@@ -53,4 +53,8 @@ class porterbox {
 	file { '/etc/cron.d/puppet-update-dchroots':
 		content  => "0 15 * * 0 root PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/sbin:/usr/local/bin chronic setup-all-dchroots\n",
 	}
+	file { '/etc/cron.weekly/puppet-mail-big-homedirs':
+		mode    => '0555',
+		source  => 'puppet:///modules/porterbox/mail-big-homedirs',
+	}
 }
