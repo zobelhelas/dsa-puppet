7e22696fbb4b41c03143532a9676aae6749329e0
Peter Palfrader <peter@palfrader.org>
2013-12-30 22:34:43 +0100

    Try to create shared keys using puppet

--- modules/named/manifests/authoritative.pp
+++ modules/named/manifests/authoritative.pp
@@ -12,4 +12,10 @@ class named::authoritative inherits named {
 		owner   => root,
 		group   => bind,
 	}
+	file { '/etc/bind/named.conf.puppet-shared-keys':
+		mode    => '0640',
+		content => template('named/named.conf.puppet-shared-keys.erb'),
+		owner   => root,
+		group   => bind,
+	}
 }
