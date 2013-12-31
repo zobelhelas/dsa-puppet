544ff05b30afca00887891d40300e37734c97498
Stephen Gran <steve@lobefin.net>
2013-12-31 16:32:50 +0000

    make cron be quiet
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/clamav/manifests/init.pp
+++ modules/clamav/manifests/init.pp
@@ -1,3 +1,11 @@
+# = Class: clamav
+#
+# Manage a clamav installation
+#
+# == Sample Usage:
+#
+#   include clamav
+#
 class clamav {
 	package { [
 			'clamav-daemon',
@@ -39,6 +47,8 @@ class clamav {
 		notify => Service['clamav-daemon']
 	}
 	file { '/var/lib/clamav/.nobackup':
+		owner   => clamav,
+		mode    => '0644',
 		content => '',
 		require => Package['clamav-daemon']
 	}
