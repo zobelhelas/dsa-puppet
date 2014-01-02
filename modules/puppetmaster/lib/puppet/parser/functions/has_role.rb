2e2a6a3b1a42ca3351676096fdaa9f642894cdc8
Stephen Gran <steve@lobefin.net>
2014-01-02 19:52:10 +0000

    try to fix errors
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/puppetmaster/lib/puppet/parser/functions/has_role.rb
+++ modules/puppetmaster/lib/puppet/parser/functions/has_role.rb
@@ -1,14 +1,12 @@
 module Puppet::Parser::Functions
   newfunction(:has_role, :type => :rvalue) do |args|
-    begin
       role = args.shift
       roles = lookupvar('site::roles')
       fqdn = lookupvar('fqdn')
       if not roles.include?(role)
         err "Failed to look up missing role #{role}"
-        return False
+        return false
       end
       return roles[role].include?(fqdn)
     end
-  end
 end
