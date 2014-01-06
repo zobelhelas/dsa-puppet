13d861e97b0253a20a07e4671affba6fb92f7324
Peter Palfrader <peter@palfrader.org>
2014-01-06 12:46:32 +0100

    tlsa record for lists

--- modules/roles/manifests/lists.pp
+++ modules/roles/manifests/lists.pp
@@ -2,4 +2,11 @@ class roles::lists {
 	ssl::service { 'lists.debian.org':
 		notify => Service['apache2'],
 	}
+
+	dnsextras::tlsa_record{ 'tlsa-mailport':
+		zone     => 'debian.org',
+		certfile => "/etc/puppet/modules/exim/files/certs/${::fqdn}.crt",
+		port     => 25,
+		hostname => $::fqdn,
+	}
 }
