2a8489ac51653487b788369edb7e15765799925a
Luca Filipozzi <lfilipoz@emyr.net>
2014-01-11 17:49:11 +0000

    remove mod state

--- modules/roles/manifests/sip.pp
+++ modules/roles/manifests/sip.pp
@@ -2,26 +2,26 @@ class roles::sip {
 	@ferm::rule { 'dsa-sip-ws':
 		domain      => 'ip',
 		description => 'SIP connections (WebSocket; for WebRTC)',
-		rule        => 'proto tcp mod state state (NEW) dport (443) ACCEPT'
+		rule        => 'proto tcp dport (443) ACCEPT'
 	}
 	@ferm::rule { 'dsa-sip-tls':
 		domain      => 'ip',
 		description => 'SIP connections (TLS)',
-		rule        => 'proto tcp mod state state (NEW) dport (5061) ACCEPT'
+		rule        => 'proto tcp dport (5061) ACCEPT'
 	}
 	@ferm::rule { 'dsa-turn':
 		domain      => 'ip',
 		description => 'TURN connections',
-		rule        => 'proto udp mod state state (NEW) dport (3478) ACCEPT'
+		rule        => 'proto udp dport (3478) ACCEPT'
 	}
 	@ferm::rule { 'dsa-turn-tls':
 		domain      => 'ip',
 		description => 'TURN connections (TLS)',
-		rule        => 'proto tcp mod state state (NEW) dport (5349) ACCEPT'
+		rule        => 'proto tcp dport (5349) ACCEPT'
 	}
 	@ferm::rule { 'dsa-rtp':
 		domain      => 'ip',
 		description => 'RTP streams',
-		rule        => 'proto udp mod state state (NEW) dport (49152:65535) ACCEPT'
+		rule        => 'proto udp dport (49152:65535) ACCEPT'
 	}
 }
