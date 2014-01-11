542dc9b494931d4e0abd26890b27f34b009f7859
Luca Filipozzi <lfilipoz@emyr.net>
2014-01-11 17:05:21 +0000

    must ACCEPT the connections, silly

--- modules/roles/manifests/sip.pp
+++ modules/roles/manifests/sip.pp
@@ -2,21 +2,21 @@ class roles::sip {
 	@ferm::rule { 'sip-ws':
 		domain      => 'ip',
 		description => 'SIP over WebSocket (for WebRTC)',
-		rule        => 'proto tcp mod state state (NEW) dport (443)'
+		rule        => 'proto tcp mod state state (NEW) dport (443) ACCEPT'
 	}
 	@ferm::rule { 'sip':
 		domain      => 'ip',
 		description => 'SIP connections (TLS)',
-		rule        => 'proto tcp mod state state (NEW) dport (5061)'
+		rule        => 'proto tcp mod state state (NEW) dport (5061) ACCEPT'
 	}
 	@ferm::rule { 'turn':
 		domain      => 'ip',
 		description => 'TURN connections (TLS)',
-		rule        => 'proto tcp mod state state (NEW) dport (5349)'
+		rule        => 'proto tcp mod state state (NEW) dport (5349) ACCEPT'
 	}
 	@ferm::rule { 'rtp':
 		domain      => 'ip',
 		description => 'RTP streams',
-		rule        => 'proto udp mod state state (NEW) dport (49152:65535)'
+		rule        => 'proto udp mod state state (NEW) dport (49152:65535) ACCEPT'
 	}
 }
