1aa4fe438f2c5b78595c2c530dd84144b948a872
Luca Filipozzi <lfilipoz@emyr.net>
2014-01-11 17:06:19 +0000

    use standardized names for rules

--- modules/roles/manifests/sip.pp
+++ modules/roles/manifests/sip.pp
@@ -1,20 +1,20 @@
 class roles::sip {
-	@ferm::rule { 'sip-ws':
+	@ferm::rule { 'dsa-sip-ws':
 		domain      => 'ip',
 		description => 'SIP over WebSocket (for WebRTC)',
 		rule        => 'proto tcp mod state state (NEW) dport (443) ACCEPT'
 	}
-	@ferm::rule { 'sip':
+	@ferm::rule { 'dsa-sip':
 		domain      => 'ip',
 		description => 'SIP connections (TLS)',
 		rule        => 'proto tcp mod state state (NEW) dport (5061) ACCEPT'
 	}
-	@ferm::rule { 'turn':
+	@ferm::rule { 'dsa-turn':
 		domain      => 'ip',
 		description => 'TURN connections (TLS)',
 		rule        => 'proto tcp mod state state (NEW) dport (5349) ACCEPT'
 	}
-	@ferm::rule { 'rtp':
+	@ferm::rule { 'dsa-rtp':
 		domain      => 'ip',
 		description => 'RTP streams',
 		rule        => 'proto udp mod state state (NEW) dport (49152:65535) ACCEPT'
