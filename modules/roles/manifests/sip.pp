63cae429ce287c42947c8a19df2dc0ea577578ed
Luca Filipozzi <lfilipoz@emyr.net>
2014-01-12 07:50:42 +0000

    enable ipv6 rules for sip

--- modules/roles/manifests/sip.pp
+++ modules/roles/manifests/sip.pp
@@ -1,26 +1,51 @@
 class roles::sip {
-	@ferm::rule { 'dsa-sip-ws':
+	@ferm::rule { 'dsa-sip-ws-ip4':
 		domain      => 'ip',
 		description => 'SIP connections (WebSocket; for WebRTC)',
 		rule        => 'proto tcp dport (443) ACCEPT'
 	}
-	@ferm::rule { 'dsa-sip-tls':
+	@ferm::rule { 'dsa-sip-ws-ip6':
+		domain      => 'ip6',
+		description => 'SIP connections (WebSocket; for WebRTC)',
+		rule        => 'proto tcp dport (443) ACCEPT'
+	}
+	@ferm::rule { 'dsa-sip-tls-ip4':
 		domain      => 'ip',
 		description => 'SIP connections (TLS)',
 		rule        => 'proto tcp dport (5061) ACCEPT'
 	}
-	@ferm::rule { 'dsa-turn':
+	@ferm::rule { 'dsa-sip-tls-ip6':
+		domain      => 'ip6',
+		description => 'SIP connections (TLS)',
+		rule        => 'proto tcp dport (5061) ACCEPT'
+	}
+	@ferm::rule { 'dsa-turn-ip4':
 		domain      => 'ip',
 		description => 'TURN connections',
 		rule        => 'proto udp dport (3478) ACCEPT'
 	}
-	@ferm::rule { 'dsa-turn-tls':
+	@ferm::rule { 'dsa-turn-ip6':
+		domain      => 'ip6',
+		description => 'TURN connections',
+		rule        => 'proto udp dport (3478) ACCEPT'
+	}
+	@ferm::rule { 'dsa-turn-tls-ip4':
 		domain      => 'ip',
 		description => 'TURN connections (TLS)',
 		rule        => 'proto tcp dport (5349) ACCEPT'
 	}
-	@ferm::rule { 'dsa-rtp':
+	@ferm::rule { 'dsa-turn-tls-ip6':
+		domain      => 'ip6',
+		description => 'TURN connections (TLS)',
+		rule        => 'proto tcp dport (5349) ACCEPT'
+	}
+	@ferm::rule { 'dsa-rtp-ip4':
 		domain      => 'ip',
+		description => 'RTP streams',
+		rule        => 'proto udp dport (49152:65535) ACCEPT'
+	}
+	@ferm::rule { 'dsa-rtp-ip6':
+		domain      => 'ip6',
 		description => 'RTP streams',
 		rule        => 'proto udp dport (49152:65535) ACCEPT'
 	}
