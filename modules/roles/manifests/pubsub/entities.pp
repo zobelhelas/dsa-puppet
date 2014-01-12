91a72d82de7cdaee0e33b49e870bf3be10adf7db
Stephen Gran <steve@lobefin.net>
2014-01-12 10:14:17 +0000

    Revert "add ssl auth support"
    This reverts commit f419a83c524efb35f70c9e0901a1289f0033b662.

--- modules/roles/manifests/pubsub/entities.pp
+++ modules/roles/manifests/pubsub/entities.pp
@@ -277,11 +277,5 @@ class roles::pubsub::entities {
 		require  => Package['rabbitmq-server'],
 		notify   => Service['rabbitmq-server']
 	}
-	rabbitmq_plugin { 'rabbitmq_auth_mechanism_ssl':
-		ensure   => present,
-		provider => 'rabbitmqplugins',
-		require  => Package['rabbitmq-server'],
-		notify   => Service['rabbitmq-server']
-	}
 
 }
