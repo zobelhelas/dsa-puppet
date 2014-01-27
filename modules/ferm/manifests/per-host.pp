69b910a5f5aac95bb20441e0e80cc7ba47ed25ca
Peter Palfrader <peter@palfrader.org>
2014-01-27 22:04:13 +0100

    syntax fix

--- modules/ferm/manifests/per-host.pp
+++ modules/ferm/manifests/per-host.pp
@@ -298,6 +298,7 @@ class ferm::per-host {
 				description     => 'Allow postgress access4',
 				rule            => '&SERVICE_RANGE(tcp, 5438, ( 206.12.19.0/24 ))'
 			}
+		}
 		chopin: {
 			@ferm::rule { 'dsa-postgres-backup':
 				# ubc, wuit
@@ -309,7 +310,6 @@ class ferm::per-host {
 				description     => 'Allow postgress access',
 				rule            => '&SERVICE_RANGE(tcp, 5432, ( 2001:41c8:1000:21::21:12/128 ))'
 			}
-
 		}
 		default: {}
 	}
