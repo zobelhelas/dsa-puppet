cefc704c6ba62d49487565d06cc00ce2aa03311d
Peter Palfrader <peter@palfrader.org>
2014-03-27 08:12:46 +0100

    try this

--- modules/hardware/manifests/raid/proliant.pp
+++ modules/hardware/manifests/raid/proliant.pp
@@ -10,7 +10,7 @@ class hardware::raid::proliant {
 		ensure  => installed,
 		tag    => extra_repo,
 	}
-	if ! "$::systemproductname" in ["ProLiant DL180 G5", "ProLiant DL120 G5"] {
+	if !("$::systemproductname" in ["ProLiant DL180 G5", "ProLiant DL120 G5"]) {
 		package { 'hp-health':
 			ensure => installed,
 			tag    => extra_repo,
