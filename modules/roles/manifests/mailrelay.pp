67cd009c15f58469d007633e610cbbe4e47c385e
Stephen Gran <steve@lobefin.net>
2014-03-31 08:10:58 +0100

    add client configs for mail relay
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- modules/roles/manifests/mailrelay.pp
+++ modules/roles/manifests/mailrelay.pp
@@ -1,2 +1,23 @@
+# = Class: roles::mailrelay
+#
+# Setup for mailrelay hosts
+#
+# == Sample Usage:
+#
+#   include roles::mailrelay
+#
 class roles::mailrelay {
+	include roles::pubsub::parameters
+
+	$rabbit_password = $roles::pubsub::parameters::rabbit_password
+
+	roles::pubsub::config { 'emailvdomains':
+		key      => 'dsa-emailvdomains-receive',
+		exchange => dsa,
+		queue    => "email-${::fqdn}",
+		topic    => 'dsa.email.update',
+		vhost    => dsa,
+		username => $::fqdn,
+		password => $rabbit_password
+	}
 }
