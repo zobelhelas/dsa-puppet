8fa77ff325d06880ddd12431ca528cf5ac997b33
Peter Palfrader <peter@palfrader.org>
2014-05-31 18:04:28 +0200

    load nfs and autofs4 on autofs hosts

--- modules/autofs/manifests/common.pp
+++ modules/autofs/manifests/common.pp
@@ -2,6 +2,9 @@ class autofs::common {
 	package { 'autofs': ensure => installed }
 	package { 'nfs-common': ensure => installed }
 
+	site::linux_module { 'nfs': }
+	site::linux_module { 'autofs4': }
+
 	exec { 'autofs reload':
 		path        => '/usr/bin:/usr/sbin:/bin:/sbin',
 		command     => 'service autofs reload',
