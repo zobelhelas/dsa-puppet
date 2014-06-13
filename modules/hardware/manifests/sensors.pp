6d6e46fc8ef35efc401a85357585bb9c21258295
Peter Palfrader <peter@palfrader.org>
2014-06-13 16:05:57 +0200

    Revert "hardware sensors munin plugin restart munin-async daemon"
    This reverts commit 2b17be6ea94945aef6426160a5b5c3152bd53432.  This
    doesn't do anything by itself so far.

--- modules/hardware/manifests/sensors.pp
+++ modules/hardware/manifests/sensors.pp
@@ -3,8 +3,4 @@ class hardware::sensors {
 		package { 'lm-sensors': ensure => installed, }
 		munin::check { 'sensors_temp': script => 'sensors_' }
 	}
-	exec { 'invoke-rc.d munin-async restart':
-		path        => '/usr/bin:/usr/sbin:/bin:/sbin',
-		refreshonly => true
-	}
 }
