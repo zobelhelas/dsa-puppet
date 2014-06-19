9476f6b0e0f2e86436f9b2656bef781534247ec0
Peter Palfrader <peter@palfrader.org>
2014-06-19 08:50:07 +0200

    Also populate the else branch of the hp-health pin choice

--- modules/hardware/manifests/init.pp
+++ modules/hardware/manifests/init.pp
@@ -5,6 +5,10 @@ class hardware {
 		file { '/etc/apt/preferences.d/dsa-hp-tools':
 			content => "Package: hp-health\nPin: version 8.6*\nPin-Priority: 1100\n"
 		}
+	} else {
+		file { '/etc/apt/preferences.d/dsa-hp-tools':
+			ensure => absent
+		}
 	}
 
 	include hardware::sensors
