7975bd61f37c3a89bb4bdbb6f5691fdac56192e4
Peter Palfrader <peter@palfrader.org>
2014-07-13 00:21:04 +0200

    autofs on lw0[78]

--- modules/autofs/manifests/init.pp
+++ modules/autofs/manifests/init.pp
@@ -3,7 +3,7 @@ class autofs {
 		pejacevic, piu-slave-bm-a, picconi, coccia, couper, dillon, donizetti, ticharich, delfin, quantz: {
 			include autofs::bytemark
 		}
-		lw05,lw06: {
+		lw05,lw06,lw07,lw08: {
 			include autofs::leaseweb
 		}
 	}
