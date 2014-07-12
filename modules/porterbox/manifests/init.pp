f9edc08d80cae5ea635ba30159133155ecdc23d7
Peter Palfrader <peter@palfrader.org>
2014-07-12 11:56:32 +0200

    Move some of the porterbox schroot logic to the schroot module that is shared with buildd

--- modules/porterbox/manifests/init.pp
+++ modules/porterbox/manifests/init.pp
@@ -1,39 +1,6 @@
 class porterbox {
 	include schroot
-	# FIXME: Refactor this to a common class when something else needs it
-	package { 'moreutils':
-		ensure => installed
-	}
 
-	file { '/etc/schroot/dsa':
-		ensure => directory,
-		require => Package['schroot'],
-	}
-	file { '/etc/schroot/dsa/config':
-		source  => 'puppet:///modules/porterbox/schroot-dsa/config',
-	}
-	if ! ($::debarchitecture in ['kfreebsd-amd64', 'kfreebsd-i386']) {
-		file { '/etc/schroot/dsa/fstab':
-			source  => 'puppet:///modules/porterbox/schroot-dsa/fstab',
-		}
-	} else {
-		file { '/etc/schroot/dsa/fstab':
-			source  => 'puppet:///modules/porterbox/schroot-dsa/fstab-freebsd',
-		}
-	}
-	file { '/etc/schroot/dsa/default-mirror':
-		content => template('porterbox/default-mirror.erb'),
-	}
-	file { '/etc/schroot/setup.d/99porterbox-extra-apt-options':
-		mode    => '0555',
-		source  => 'puppet:///modules/porterbox/schroot-setup.d/99porterbox-extra-apt-options',
-		require => Package['schroot'],
-	}
-	file { '/etc/schroot/setup.d/99porterbox-extra-sources':
-		mode    => '0555',
-		source  => 'puppet:///modules/porterbox/schroot-setup.d/99porterbox-extra-sources',
-		require => Package['schroot'],
-	}
 	file { '/usr/local/bin/dd-schroot-cmd':
 		mode    => '0555',
 		source  => 'puppet:///modules/porterbox/dd-schroot-cmd',
--- modules/schroot/manifests/init.pp
+++ modules/schroot/manifests/init.pp
@@ -6,18 +6,47 @@ class schroot {
 	package { 'debootstrap':
 		ensure => installed,
 	}
-
-	file { '/etc/schroot/mount-defaults':
-		content => template('schroot/etc/schroot/mount-defaults.erb'),
-		require => Package['schroot'],
+	package { 'moreutils':
+		ensure => installed
 	}
+
 	file { '/etc/default/schroot':
 		source  => 'puppet:///modules/schroot/default-schroot',
 		require => Package['schroot']
 	}
 
+	file { '/etc/schroot/mount-defaults':
+		content => template('schroot/mount-defaults.erb'),
+		require => Package['schroot'],
+	}
 	file { '/etc/schroot/default/nssdatabases':
 		source  => 'puppet:///modules/schroot/nssdatabases',
 		require => Package['schroot']
+	}
+
+	file { '/etc/schroot/setup.d/99porterbox-extra-apt-options':
+		mode    => '0555',
+		source  => 'puppet:///modules/schroot/schroot-setup.d/99porterbox-extra-apt-options',
+		require => Package['schroot'],
+	}
+	file { '/etc/schroot/setup.d/99porterbox-extra-sources':
+		mode    => '0555',
+		source  => 'puppet:///modules/schroot/schroot-setup.d/99porterbox-extra-sources',
+		require => Package['schroot'],
+	}
+
+	file { '/etc/schroot/dsa':
+		ensure => directory,
+		require => Package['schroot'],
+	}
+	file { '/etc/schroot/dsa/default-mirror':
+		content => template('schroot/default-mirror.erb'),
+	}
+	file { '/etc/schroot/dsa/config':
+		source  => 'puppet:///modules/schroot/schroot-dsa/config',
+	}
+	file { '/etc/schroot/dsa/fstab':
+		content => template('schroot/schroot-dsa/fstab.erb'),
+		require => Package['schroot'],
 	}
 }
