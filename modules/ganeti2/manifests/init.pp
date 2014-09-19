b85152452204fdacaa807997158c1980dcb26ebe
Peter Palfrader <peter@palfrader.org>
2014-09-19 12:20:10 +0200

    See if we still need instance-debootstrap

--- modules/ganeti2/manifests/init.pp
+++ modules/ganeti2/manifests/init.pp
@@ -17,10 +17,6 @@ class ganeti2 {
 		ensure => installed
 	}
 
-	package { 'ganeti-instance-debootstrap':
-		ensure => installed
-	}
-
 	if $drbd {
 		package { 'drbd8-utils':
 			ensure => installed
@@ -28,53 +24,4 @@ class ganeti2 {
 	}
 
 	site::linux_module { 'tun': }
-
-	file {
-		'/etc/ganeti/instance-debootstrap/variants.list':
-			content => template('ganeti2/instance-debootstrap/variants.list.erb'),
-			require => Package['ganeti-instance-debootstrap'],
-			;
-		'/etc/ganeti/instance-debootstrap/variants/dsa.conf':
-			content => template('ganeti2/instance-debootstrap/variants/dsa.conf.erb'),
-			require => Package['ganeti-instance-debootstrap'],
-			;
-		'/etc/ganeti/instance-debootstrap/variants/dsa-wheezy.conf':
-			content => template('ganeti2/instance-debootstrap/variants/dsa-wheezy.conf.erb'),
-			require => Package['ganeti-instance-debootstrap'],
-			;
-		'/etc/ganeti/instance-debootstrap/hooks/00-dsa-configure-networking':
-			content => template('ganeti2/instance-debootstrap/hooks/00-dsa-configure-networking.erb'),
-			require => Package['ganeti-instance-debootstrap'],
-			mode    => '0555',
-			;
-		'/etc/ganeti/instance-debootstrap/hooks/10-dsa-install-extra-packages':
-			content => template('ganeti2/instance-debootstrap/hooks/10-dsa-install-extra-packages.erb'),
-			require => Package['ganeti-instance-debootstrap'],
-			mode    => '0555',
-			;
-		'/etc/ganeti/instance-debootstrap/hooks/20-dsa-install-bootloader':
-			content => template('ganeti2/instance-debootstrap/hooks/20-dsa-install-bootloader.erb'),
-			require => Package['ganeti-instance-debootstrap'],
-			mode    => '0555',
-			;
-		'/etc/ganeti/instance-debootstrap/hooks/30-dsa-install-ssh-keys':
-			content => template('ganeti2/instance-debootstrap/hooks/30-dsa-install-ssh-keys.erb'),
-			require => Package['ganeti-instance-debootstrap'],
-			mode    => '0555',
-			;
-		'/etc/ganeti/instance-debootstrap/hooks/40-dsa-setup-swapfile':
-			content => template('ganeti2/instance-debootstrap/hooks/40-dsa-setup-swapfile.erb'),
-			require => Package['ganeti-instance-debootstrap'],
-			mode    => '0555',
-			;
-		'/etc/ganeti/instance-debootstrap/hooks/clear-root-password':
-			require => Package['ganeti-instance-debootstrap'],
-			mode    => '0444',
-			;
-		'/etc/ganeti/instance-debootstrap/hooks/xen-hvc0':
-			require => Package['ganeti-instance-debootstrap'],
-			mode    => '0444',
-			;
-	}
-
 }
