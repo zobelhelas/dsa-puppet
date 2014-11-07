bdccdb17b1bb9be3e74bb63e91eac083deab144c
Peter Palfrader <peter@palfrader.org>
2014-11-07 20:53:11 +0100

    update-alternatives is now in usr/bin

--- modules/site/manifests/alternative.pp
+++ modules/site/manifests/alternative.pp
@@ -1,12 +1,12 @@
 define site::alternative ($linkto, $ensure = present) {
 	case $ensure {
 		present: {
-			exec { "/usr/sbin/update-alternatives --set ${name} ${linkto}":
+			exec { "/usr/bin/update-alternatives --set ${name} ${linkto}":
 				unless => "[ $(update-alternatives --query ${name} | grep ^Value | awk '{print \$2}') = ${linkto} ]",
 			}
 		}
 		absent: {
-			exec { "/usr/sbin/update-alternatives --remove ${name} ${linkto}":
+			exec { "/usr/bin/update-alternatives --remove ${name} ${linkto}":
 				unless => "[ $(update-alternatives --query ${name} | grep ^Value | awk '{print \$2}') != ${linkto} ]",
 			}
 		}
