2c2879ca87a77dbdcc85401511ae9bc02a673348
Evgeni Golov <evgeni@debian.org>
2014-11-09 15:56:10 +0100

    fix typo: it's secretfile, not seccretfile
    Signed-off-by: Peter Palfrader <peter@palfrader.org>

--- modules/puppetmaster/lib/puppet/parser/functions/hmac.rb
+++ modules/puppetmaster/lib/puppet/parser/functions/hmac.rb
@@ -8,7 +8,7 @@ module Puppet::Parser::Functions
     begin
       secret = File.new(secretfile, "r").read
     rescue => e
-      raise Puppet::ParseError, "Error loading secret from #{seccretfile}: #{e.message}\n#{e.backtrace}"
+      raise Puppet::ParseError, "Error loading secret from #{secretfile}: #{e.message}\n#{e.backtrace}"
     end
 
     return OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new('sha256'), secret, data)
