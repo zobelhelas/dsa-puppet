599ad0b40f5cb2bc41525af4e5fba5a4a153decb
Luca Filipozzi <lfilipoz@emyr.net>
2015-02-13 18:48:30 +0000

    remove rossini and salieri

--- modules/multipath/manifests/init.pp
+++ modules/multipath/manifests/init.pp
@@ -14,7 +14,7 @@ class multipath {
 		bm-bl1,bm-bl2,bm-bl3,bm-bl4,bm-bl5,bm-bl6,bm-bl7,bm-bl8,bm-bl13,bm-bl14: {
 			$conffile = 'multipath-bm.conf'
 		}
-		dijkstra,luchesi,rossini,salieri,pasquini,tristano,bertali: {
+		dijkstra,luchesi,pasquini,tristano,bertali: {
 			$conffile = 'multipath-ubc-ganeti.conf'
 		}
 		default: {
