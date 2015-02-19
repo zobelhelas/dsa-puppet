4e8fd0729fc6881f65ba8be220314b6507ad9436
Peter Palfrader <peter@palfrader.org>
2015-02-19 20:37:15 +0100

    bm autofs for lindsay

--- modules/autofs/manifests/init.pp
+++ modules/autofs/manifests/init.pp
@@ -1,6 +1,6 @@
 class autofs {
 	case $::hostname {
-		pejacevic, piu-slave-bm-a, picconi, coccia, couper, dillon, donizetti, ticharich, delfin, quantz, sor: {
+		pejacevic, piu-slave-bm-a, picconi, coccia, couper, dillon, donizetti, ticharich, delfin, quantz, sor, lindsay: {
 			include autofs::bytemark
 		}
 		lw07,lw08: {
