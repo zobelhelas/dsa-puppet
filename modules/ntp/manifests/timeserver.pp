d2e73a9e90b6d671c545ba2c552239bf55e1f20f
Paul Wise <pabs@debian.org>
2015-04-27 07:53:18 +0800

    Use the leap-seconds.list file from tzdata on jessie (see #775166)
    Allows us to get rid of the leap-seconds.list embedded data copy.

--- modules/ntp/manifests/timeserver.pp
+++ modules/ntp/manifests/timeserver.pp
@@ -1,7 +1,13 @@
 class ntp::timeserver {
-	file { '/var/lib/ntp/leap-seconds.list':
-		source  => 'puppet:///modules/ntp/leap-seconds.list',
-		require => Package['ntp'],
-		notify  => Service['ntp'],
+	if ($::lsbmajdistrelease >= 8) {
+		file { '/var/lib/ntp/leap-seconds.list':
+			ensure => absent,
+		}
+	} else {
+		file { '/var/lib/ntp/leap-seconds.list':
+			source  => 'puppet:///modules/ntp/leap-seconds.list',
+			require => Package['ntp'],
+			notify  => Service['ntp'],
+		}
 	}
 }
