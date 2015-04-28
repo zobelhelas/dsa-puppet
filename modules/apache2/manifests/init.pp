767498dfd5063c8c951a131426796c0d3d3bfb6c
Julien Cristau <jcristau@debian.org>
2015-04-28 17:37:56 +0200

    apache2: configure mpm_worker module on jessie hosts
    Signed-off-by: Julien Cristau <jcristau@debian.org>

--- modules/apache2/manifests/init.pp
+++ modules/apache2/manifests/init.pp
@@ -87,6 +87,12 @@ class apache2 {
 		source => 'puppet:///modules/apache2/pratchett',
 	}
 
+	if $::lsbmajdistrelease > 7 {
+		file { '/etc/apache2/mods-available/mpm_worker.conf':
+			source => 'puppet:///modules/apache2/mpm_worker',
+		}
+	}
+
 	file { '/etc/apache2/sites-available/common-ssl.inc':
 		ensure => absent,
 	}
