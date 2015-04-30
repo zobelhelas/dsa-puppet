19d1e648631f973a60cd2737049e233b16692e1c
Paul Wise <pabs@debian.org>
2015-04-30 08:35:51 +0800

    Revert "Use the leap-seconds.list file from tzdata on jessie (see #775166)"
    This reverts commit d2e73a9e90b6d671c545ba2c552239bf55e1f20f.
    
    Too early!

--- modules/ntp/manifests/timeserver.pp
+++ modules/ntp/manifests/timeserver.pp
@@ -1,13 +1,7 @@
 class ntp::timeserver {
-	if ($::lsbmajdistrelease >= 8) {
-		file { '/var/lib/ntp/leap-seconds.list':
-			ensure => absent,
-		}
-	} else {
-		file { '/var/lib/ntp/leap-seconds.list':
-			source  => 'puppet:///modules/ntp/leap-seconds.list',
-			require => Package['ntp'],
-			notify  => Service['ntp'],
-		}
+	file { '/var/lib/ntp/leap-seconds.list':
+		source  => 'puppet:///modules/ntp/leap-seconds.list',
+		require => Package['ntp'],
+		notify  => Service['ntp'],
 	}
 }
