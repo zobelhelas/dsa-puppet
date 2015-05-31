abf5299108b021bebf2fb9cd7519898e3baf29bd
Martin Zobel-Helas <zobel@debian.org>
2015-05-31 17:00:18 +0000

    move to puppetlabs rabbitmq module
    Signed-off-by: Martin Zobel-Helas <zobel@debian.org>

--- modules/roles/manifests/pubsub/entities.pp
+++ modules/roles/manifests/pubsub/entities.pp
@@ -21,243 +21,175 @@ class roles::pubsub::entities {
 	rabbitmq_user { 'admin':
 		admin    => true,
 		password => $admin_password,
-		provider => 'rabbitmqctl',
 	}
 
 	rabbitmq_user { 'ftpteam':
 		admin    => false,
 		password => $ftp_password,
-		provider => 'rabbitmqctl',
 	}
 
 	rabbitmq_user { 'buildd':
 		admin    => false,
 		password => $buildd_password,
-		provider => 'rabbitmqctl',
 	}
 
 	rabbitmq_user { 'wbadm':
 		admin    => false,
 		password => $wbadm_password,
-		provider => 'rabbitmqctl',
 	}
 
 	rabbitmq_user { 'mailadm':
 		admin    => false,
 		password => $mailadm_password,
-		provider => 'rabbitmqctl',
 	}
 
 	rabbitmq_user { 'mailly':
 		admin    => false,
 		password => $mailly_password,
-		provider => 'rabbitmqctl',
 	}
 
 	rabbitmq_user { 'muffat':
 		admin    => false,
 		password => $muffat_password,
-		provider => 'rabbitmqctl',
 	}
 
 	rabbitmq_user { 'pet-devel':
 		admin    => false,
 		password => $pet_password,
-		provider => 'rabbitmqctl',
 	}
-
-	$do_hosts = keys($site::localinfo)
-
-	rabbitmq::autouser { $do_hosts: }
 
 	rabbitmq_vhost { 'packages':
 		ensure   => present,
-		provider => 'rabbitmqctl',
 	}
 
 	rabbitmq_vhost { 'buildd':
 		ensure   => present,
-		provider => 'rabbitmqctl',
 	}
 
 	rabbitmq_vhost { 'dsa':
 		ensure   => present,
-		provider => 'rabbitmqctl',
 	}
 
 	rabbitmq_vhost { 'pet':
 		ensure   => present,
-		provider => 'rabbitmqctl',
 	}
 
 	rabbitmq_user_permissions { 'admin@/':
 		configure_permission => '.*',
 		read_permission      => '.*',
 		write_permission     => '.*',
-		provider             => 'rabbitmqctl',
-		require              => Rabbitmq_user['admin']
 	}
 
 	rabbitmq_user_permissions { 'admin@buildd':
 		configure_permission => '.*',
 		read_permission      => '.*',
 		write_permission     => '.*',
-		provider             => 'rabbitmqctl',
-		require              => [
-			Rabbitmq_user['admin'],
-			Rabbitmq_vhost['buildd']
-		]
 	}
 
 	rabbitmq_user_permissions { 'admin@dsa':
 		configure_permission => '.*',
 		read_permission      => '.*',
 		write_permission     => '.*',
-		provider             => 'rabbitmqctl',
-		require              => [
-			Rabbitmq_user['admin'],
-			Rabbitmq_vhost['dsa']
-		]
 	}
 
 	rabbitmq_user_permissions { 'admin@packages':
 		configure_permission => '.*',
 		read_permission      => '.*',
 		write_permission     => '.*',
-		provider             => 'rabbitmqctl',
-		require              => [
-			Rabbitmq_user['admin'],
-			Rabbitmq_vhost['packages']
-		]
 	}
 
 	rabbitmq_user_permissions { 'admin@pet':
 		configure_permission => '.*',
 		read_permission      => '.*',
 		write_permission     => '.*',
-		provider             => 'rabbitmqctl',
-		require              => [
-			Rabbitmq_user['admin'],
-			Rabbitmq_vhost['pet']
-		]
 	}
 
 	rabbitmq_user_permissions { 'ftpteam@packages':
 		configure_permission => '.*',
 		read_permission      => '.*',
 		write_permission     => '.*',
-		provider             => 'rabbitmqctl',
-		require              => [
-			Rabbitmq_user['ftpteam'],
-			Rabbitmq_vhost['packages']
-		]
 	}
 
 	rabbitmq_user_permissions { 'wbadm@packages':
 		read_permission      => 'unchecked',
 		write_permission     => 'wbadm',
-		provider             => 'rabbitmqctl',
-		require              => [
-			Rabbitmq_user['wbadm'],
-			Rabbitmq_vhost['packages']
-		]
 	}
 
 	rabbitmq_user_permissions { 'buildd@buildd':
 		configure_permission => '.*',
 		read_permission      => '.*',
 		write_permission     => '.*',
-		provider             => 'rabbitmqctl',
-		require              => [
-			Rabbitmq_user['buildd'],
-			Rabbitmq_vhost['buildd']
-		]
 	}
 
 	rabbitmq_user_permissions { 'wbadm@buildd':
 		configure_permission => '.*',
 		read_permission      => '.*',
 		write_permission     => '.*',
-		provider             => 'rabbitmqctl',
-		require              => [
-			Rabbitmq_user['wbadm'],
-			Rabbitmq_vhost['buildd']
-		]
 	}
 
 	rabbitmq_user_permissions { 'mailadm@dsa':
 		configure_permission => '.*',
 		read_permission      => '.*',
 		write_permission     => '.*',
-		provider             => 'rabbitmqctl',
-		require              => [
-			Rabbitmq_user['mailadm'],
-			Rabbitmq_vhost['dsa']
-		]
 	}
 
 	rabbitmq_user_permissions { 'pet-devel@pet':
 		configure_permission => '.*',
 		read_permission      => '.*',
 		write_permission     => '.*',
-		provider             => 'rabbitmqctl',
-		require              => [
-			Rabbitmq_user['pet-devel'],
-			Rabbitmq_vhost['pet']
-		]
 	}
 
-	rabbitmq_policy { 'mirror-dsa':
-		vhost   => 'dsa',
-		match   => '.*',
-		policy  => '{"ha-mode":"all"}',
-		require => Rabbitmq_vhost['dsa']
+	rabbitmq_policy { 'mirror-dsa@dsa':
+		pattern    => '.*',
+		priority   => 0,
+		applyto    => 'all',
+		definition => {
+			'ha-mode'      => 'all',
+			'ha-sync-mode' => 'automatic',
+		},
 	}
 
-	rabbitmq_policy { 'mirror-buildd':
-		vhost   => 'buildd',
-		match   => '.*',
-		policy  => '{"ha-mode":"all"}',
-		require => Rabbitmq_vhost['buildd']
+	rabbitmq_policy { 'mirror-buildd@buildd':
+		pattern    => '.*',
+		priority   => 0,
+		applyto    => 'all',
+		definition => {
+			'ha-mode'      => 'all',
+			'ha-sync-mode' => 'automatic',
+		},
 	}
 
-	rabbitmq_policy { 'mirror-packages':
-		vhost   => 'packages',
-		match   => '.*',
-		policy  => '{"ha-mode":"all"}',
-		require => Rabbitmq_vhost['packages']
+	rabbitmq_policy { 'mirror-packages@packages':
+		pattern    => '.*',
+		priority   => 0,
+		applyto    => 'all',
+		definition => {
+			'ha-mode'      => 'all',
+			'ha-sync-mode' => 'automatic',
+		},
 	}
 
-	rabbitmq_policy { 'mirror_pet':
-		vhost   => 'pet',
-		match   => '.*',
-		policy  => '{"ha-mode":"all"}',
-		require => Rabbitmq_vhost['pet']
+	rabbitmq_policy { 'mirror_pet@pet':
+		pattern    => '.*',
+		priority   => 0,
+		applyto    => 'all',
+		definition => {
+			'ha-mode'      => 'all',
+			'ha-sync-mode' => 'automatic',
+		},
 	}
 
 	rabbitmq_plugin { 'rabbitmq_management':
 		ensure   => present,
-		provider => 'rabbitmqplugins',
-		require  => Package['rabbitmq-server'],
-		notify   => Service['rabbitmq-server']
 	}
 	rabbitmq_plugin { 'rabbitmq_management_agent':
 		ensure   => present,
-		provider => 'rabbitmqplugins',
-		require  => Package['rabbitmq-server'],
-		notify   => Service['rabbitmq-server']
 	}
 	rabbitmq_plugin { 'rabbitmq_tracing':
 		ensure   => present,
-		provider => 'rabbitmqplugins',
-		require  => Package['rabbitmq-server'],
-		notify   => Service['rabbitmq-server']
 	}
 	rabbitmq_plugin { 'rabbitmq_management_visualiser':
 		ensure   => present,
-		provider => 'rabbitmqplugins',
-		require  => Package['rabbitmq-server'],
-		notify   => Service['rabbitmq-server']
 	}
 
 }
