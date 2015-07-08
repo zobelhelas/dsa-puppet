8789f53b6c79e9d6fc2b965ac4f5348cbb2f4a89
Peter Palfrader <peter@palfrader.org>
2015-07-08 21:15:05 +0200

    Add pinel to nfs

--- modules/autofs/manifests/init.pp
+++ modules/autofs/manifests/init.pp
@@ -1,6 +1,6 @@
 class autofs {
 	case $::hostname {
-		pejacevic, piu-slave-bm-a, picconi, coccia, couper, dillon, donizetti, ticharich, delfin, quantz, sor, lindsay, mekeel, pittar: {
+		pejacevic, piu-slave-bm-a, picconi, coccia, couper, dillon, donizetti, ticharich, delfin, quantz, sor, lindsay, mekeel, pittar, pinel: {
 			include autofs::bytemark
 		}
 		lw07,lw08: {
