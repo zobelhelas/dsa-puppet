5c238454bf1dc71195fddac8eaaf83d89da4930b
Peter Palfrader <peter@palfrader.org>
2015-08-20 09:15:26 +0200

    create portforwarder key

--- modules/portforwarder/manifests/init.pp
+++ modules/portforwarder/manifests/init.pp
@@ -1,6 +1,14 @@
 class portforwarder {
 	# do not depend on xinetd, yet.  it might uninstall other inetds
 	# for now this will have to be done manually
+
+	if ! $::portforwarder_key {
+		exec { 'create-portforwarder-key':
+			command => '/bin/su - portforwarder -c \'mkdir -p -m 02700 .ssh && ssh-keygen -C "`whoami`@`hostname` (`date +%Y-%m-%d`)" -P "" -f .ssh/id_rsa -q\'',
+			onlyif  => '/usr/bin/getent passwd portforwarder > /dev/null && ! [ -e /home/portforwarder/.ssh/id_rsa ]'
+		}
+	}
+
 	file { '/etc/ssh/userkeys/portforwarder':
 		content => template('portforwarder/authorized_keys.erb'),
 	}
