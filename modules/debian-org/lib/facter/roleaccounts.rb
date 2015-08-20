6bda2aad4f21dce071235673e366b45426df368d
Peter Palfrader <peter@palfrader.org>
2015-08-20 09:03:25 +0200

    portforwarder facters

--- modules/debian-org/lib/facter/roleaccounts.rb
+++ modules/debian-org/lib/facter/roleaccounts.rb
@@ -75,6 +75,31 @@ begin
         end
     end
 
+    Facter.add("portforwarder_key") do
+        setcode do
+            key = nil
+            keyfile = '/home/portforwarder/.ssh/id_rsa.pub'
+            if FileTest.exist?(keyfile)
+                key = File.open(keyfile).read.chomp
+            end
+            key
+        end
+    end
+
+    Facter.add("portforwarder_user_exists") do
+        setcode do
+            result = ''
+            begin
+                if Etc.getpwnam('portforwarder')
+                    result = true
+                end
+            rescue ArgumentError
+            end
+            result
+        end
+    end
+
+
 
 rescue Exception => e
 end
