6731e1546376a0d8eaec88e842693a28b7840765
Peter Palfrader <peter@palfrader.org>
2015-08-26 10:28:18 +0200

    ca.pem -> ca.crt

--- modules/roles/manifests/sso_rp.pp
+++ modules/roles/manifests/sso_rp.pp
@@ -10,8 +10,8 @@ class roles::sso_rp {
 	file { '/var/lib/dsa/sso/ca.crl':
 		content => template('roles/sso_rp/ca.crl.erb'),
 	}
-	file { '/var/lib/dsa/sso/ca.pem':
-		source => 'puppet:///modules/roles/sso_rp/ca.pem',
+	file { '/var/lib/dsa/sso/ca.crt':
+		source => 'puppet:///modules/roles/sso_rp/ca.crt',
 	}
 
 }
