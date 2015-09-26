ef6096766cebab114509587d10d16afb39bd15b1
Peter Palfrader <peter@palfrader.org>
2015-09-26 13:22:22 +0200

    make base backups on backuphost at a different time

--- modules/postgres/manifests/backup_server.pp
+++ modules/postgres/manifests/backup_server.pp
@@ -10,7 +10,13 @@ class postgres::backup_server {
 		source => 'puppet:///modules/postgres/backup_server/postgres-make-base-backups',
 		mode   => '0555',
 	}
-	file { '/etc/cron.d/puppet-postgres-make-base-backups':
-		content  => "20 0 * * 6 debbackup chronic /usr/local/bin/postgres-make-base-backups\n",
+	if $::hostname in [backuphost] {
+		file { '/etc/cron.d/puppet-postgres-make-base-backups':
+			content  => "20 1 * * 0 debbackup chronic /usr/local/bin/postgres-make-base-backups\n",
+		}
+	} else  {
+		file { '/etc/cron.d/puppet-postgres-make-base-backups':
+			content  => "20 0 * * 6 debbackup chronic /usr/local/bin/postgres-make-base-backups\n",
+		}
 	}
 }
