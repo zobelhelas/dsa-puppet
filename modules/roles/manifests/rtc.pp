20837932686b93b71481bb8ee38c902d06b25fa2
Paul Wise <pabs@debian.org>
2015-10-23 14:32:52 +0800

    Ensure that rtc's monit configuration file is absent
    Signed-off-by: Paul Wise <pabs@debian.org>

--- modules/roles/manifests/rtc.pp
+++ modules/roles/manifests/rtc.pp
@@ -107,4 +107,8 @@ class roles::rtc {
 		description => 'RTP streams',
 		rule        => 'proto udp dport (49152:65535) ACCEPT'
 	}
+
+	file { '/etc/monit/monit.d/50rtc':
+		ensure  => absent,
+	}
 }
