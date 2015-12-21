b2dda249b749f92ac561fe6c15d858b4a39439dc
Peter Palfrader <peter@palfrader.org>
2015-12-21 20:18:14 +0100

    Add more paths

--- modules/debian-org/lib/facter/paths.rb
+++ modules/debian-org/lib/facter/paths.rb
@@ -2,6 +2,10 @@
 %w{/srv/build-trees
    /srv/buildd
    /etc/ssh/ssh_host_ed25519_key
+   /srv/mirrors/debian
+   /srv/mirrors/debian-backports
+   /srv/mirrors/debian-debug
+   /srv/mirrors/debian-security
 }.each do |path|
 	Facter.add("has" + path.gsub('/','_')) do
 		setcode do
