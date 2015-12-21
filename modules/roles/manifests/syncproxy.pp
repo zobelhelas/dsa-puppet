e5ea3b52c098a2bc284e3cb22a0e0527a8764386
Peter Palfrader <peter@palfrader.org>
2015-12-21 20:09:10 +0100

    Add mirror-isc to syncproxy role, update rsync.conf

--- modules/roles/manifests/syncproxy.pp
+++ modules/roles/manifests/syncproxy.pp
@@ -3,10 +3,12 @@ class roles::syncproxy {
 		source => 'puppet:///modules/roles/syncproxy/rsyncd.conf',
 		bind   => $::hostname ? {
 			'milanollo' => '5.153.231.9',
+			'mirror-isc' => '149.20.20.21',
 			default => ''
 		},
 		bind6   => $::hostname ? {
 			'milanollo' => '2001:41c8:1000:21::21:9',
+			'mirror-isc' => '2001:4f8:8:36::1deb:21',
 			default => ''
 		},
 	}
