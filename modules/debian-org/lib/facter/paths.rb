3f2300fe6fd8dbb91c842e4a3b8c58322b65ede6
Peter Palfrader <peter@palfrader.org>
2015-12-21 20:18:07 +0100

    refactor debian-org/lib/facter/paths.rb facter

--- modules/debian-org/lib/facter/paths.rb
+++ modules/debian-org/lib/facter/paths.rb
@@ -1,36 +1,15 @@
-Facter.add("has_srv_build_trees") do
-	setcode do
-		if FileTest.exist?("/srv/build-trees")
-			true
-		else
-			''
-		end
-	end
-end
-Facter.add("has_srv_buildd") do
-	setcode do
-		if FileTest.exist?("/srv/buildd")
-			true
-		else
-			''
-		end
-	end
-end
-Facter.add("has_srv_buildd") do
-	setcode do
-		if FileTest.exist?("/srv/buildd")
-			true
-		else
-			''
-		end
-	end
-end
-Facter.add("has_etc_ssh_ssh_host_ed25519_key") do
-	setcode do
-		if FileTest.exist?("/etc/ssh/ssh_host_ed25519_key")
-			true
-		else
-			''
+
+%w{/srv/build-trees
+   /srv/buildd
+   /etc/ssh/ssh_host_ed25519_key
+}.each do |path|
+	Facter.add("has" + path.gsub('/','_')) do
+		setcode do
+			if FileTest.exist?(path)
+				true
+			else
+				''
+			end
 		end
 	end
 end
