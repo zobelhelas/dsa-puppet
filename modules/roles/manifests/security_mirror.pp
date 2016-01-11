622fcf7cc4d58bddec10e894cc3daf8efb6fca91
Peter Palfrader <peter@palfrader.org>
2016-01-11 12:51:31 +0100

    have security rsync bind to specific address

--- modules/roles/manifests/security_mirror.pp
+++ modules/roles/manifests/security_mirror.pp
@@ -1,10 +1,12 @@
 class roles::security_mirror {
 	$rsync_bind = $::hostname ? {
+		mirror-anu => '150.203.164.39', # XXX this will change to 61
 		mirror-isc => '149.20.20.19',
 		mirror-umn => '128.101.240.215',
 		default    => '',
 	}
 	$rsync_bind6 = $::hostname ? {
+		mirror-anu => '2001:388:1034:2900::3d', # XXX this will change to 3d
 		mirror-isc => '2001:4f8:8:36::1deb:19',
 		mirror-umn => '2607:ea00:101:3c0b::1deb:215',
 		default    => '',
