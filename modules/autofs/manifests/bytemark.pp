9d9ddf48c69f78d9246352fd132179fcb8ebd844
Peter Palfrader <peter@palfrader.org>
2016-01-22 21:01:44 +0100

    bytemark has debian-debug

--- modules/autofs/manifests/bytemark.pp
+++ modules/autofs/manifests/bytemark.pp
@@ -13,5 +13,6 @@ class autofs::bytemark {
 	file { '/srv/mirrors': ensure  => directory }
 	file { '/srv/mirrors/debian': ensure  => '/auto.dsa/debian' }
 	file { '/srv/mirrors/debian-backports': ensure  => '/auto.dsa/debian-backports' }
+	file { '/srv/mirrors/debian-debug': ensure  => '/auto.dsa/debian-debug' }
 	file { '/srv/mirrors/debian-security': ensure  => '/auto.dsa/debian-security' }
 }
