30ded286aa613ac41cb61d3c028f9ad1aed87cc8
Peter Palfrader <peter@palfrader.org>
2016-01-31 20:09:51 +0100

    enable ssl

--- modules/roles/manifests/syncproxy.pp
+++ modules/roles/manifests/syncproxy.pp
@@ -41,6 +41,7 @@ class roles::syncproxy {
 	}
 
 	if $::apache2 and $syncproxy_name != 'unknown' {
+		apache2::module { 'ssl': }
 		ssl::service { "$syncproxy_name": notify => Service['apache2'], key => true, }
 		apache2::site { '010-syncproxy.debian.org':
 			site   => 'syncproxy.debian.org',
