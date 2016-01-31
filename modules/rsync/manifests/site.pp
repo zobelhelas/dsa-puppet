447a9b57ca618b862e34289183d7771e729aea7f
Peter Palfrader <peter@palfrader.org>
2016-01-31 22:40:20 +0100

    one place missed

--- modules/rsync/manifests/site.pp
+++ modules/rsync/manifests/site.pp
@@ -42,7 +42,7 @@ define rsync::site (
 		bind        => $bind,
 		id          => "${name}-rsync",
 		server      => '/usr/bin/rsync',
-		port        => 'rsync',
+		service     => 'rsync',
 		server_args => "--daemon --config=${fname_real}",
 		ferm        => false,
 		instances   => $max_clients,
