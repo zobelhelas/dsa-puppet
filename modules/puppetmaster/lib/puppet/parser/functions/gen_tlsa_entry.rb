377414d029f76cd4bd9940e0bdf392cdbb852c40
Peter Palfrader <peter@palfrader.org>
2016-01-31 17:57:55 +0100

    handle array of certfiles

--- modules/puppetmaster/lib/puppet/parser/functions/gen_tlsa_entry.rb
+++ modules/puppetmaster/lib/puppet/parser/functions/gen_tlsa_entry.rb
@@ -10,23 +10,31 @@ module Puppet::Parser::Functions
       ports = [port]
     end
 
-    res = []
-    res << "; cert #{certfile} for #{hostname}:#{ports}."
-    ports.each do |port|
-      if File.exist?(certfile)
-        cmd = ['swede', 'create', '--usage=3', '--selector=1', '--mtype=1', '--certificate', certfile, '--port', port.to_s, hostname]
-        IO.popen(cmd, "r") {|i| res << i.read }
-      else
-        res << "; certfile #{certfile} did not exist to create TLSA record for #{hostname}:#{port}."
-      end
+    if certfile.kind_of?(Array)
+      certs = certfile
+    else
+      certs = [ certfile ]
+    end
 
-      cfnew = certfile.gsub(/\.crt$/, '-new.crt')
-      if cfnew != certfile and File.exist?(cfnew)
-        cmd = ['swede', 'create', '--usage=3', '--selector=1', '--mtype=1', '--certificate', cfnew, '--port', port.to_s, hostname]
-        new_entry = ''
-        IO.popen(cmd, "r") {|i| new_entry = i.read }
-        if not res.include?(new_entry)
-          res << new_entry
+    res = []
+    certs.each do |certfile|
+      res << "; cert #{certfile} for #{hostname}:#{ports}."
+      ports.each do |port|
+        if File.exist?(certfile)
+          cmd = ['swede', 'create', '--usage=3', '--selector=1', '--mtype=1', '--certificate', certfile, '--port', port.to_s, hostname]
+          IO.popen(cmd, "r") {|i| res << i.read }
+        else
+          res << "; certfile #{certfile} did not exist to create TLSA record for #{hostname}:#{port}."
+        end
+
+        cfnew = certfile.gsub(/\.crt$/, '-new.crt')
+        if cfnew != certfile and File.exist?(cfnew)
+          cmd = ['swede', 'create', '--usage=3', '--selector=1', '--mtype=1', '--certificate', cfnew, '--port', port.to_s, hostname]
+          new_entry = ''
+          IO.popen(cmd, "r") {|i| new_entry = i.read }
+          if not res.include?(new_entry)
+            res << new_entry
+          end
         end
       end
     end
