0894ec394f74db1488e2fed5a3276d468b9cc2b9
Peter Palfrader <peter@palfrader.org>
2016-02-01 22:34:34 +0100

    rsync ssl on ftp-master

--- modules/roles/manifests/ftp_master.pp
+++ modules/roles/manifests/ftp_master.pp
@@ -1,7 +1,8 @@
 class roles::ftp_master {
 	rsync::site { 'dakmaster':
 		source        => 'puppet:///modules/roles/dakmaster/rsyncd.conf',
-		max_clients => 100,
+		max_clients   => 100,
+		sslname       => 'ftp-master.debian.org',
 	}
 
 	ssl::service { 'ftp-master.debian.org':
