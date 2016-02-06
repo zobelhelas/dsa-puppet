34500ff944848374edaac6d25d5d802dafe3f6ff
Peter Palfrader <peter@palfrader.org>
2016-02-06 17:46:32 +0100

    remove www-master rsync

--- modules/roles/manifests/www_master.pp
+++ modules/roles/manifests/www_master.pp
@@ -1,7 +1,4 @@
 class roles::www_master {
-	rsync::site { 'www_master':
-		source => 'puppet:///modules/roles/www_master/rsyncd.conf',
-	}
 	ssl::service { 'www-master.debian.org':
 		notify => Service['apache2'],
 		key => true,
