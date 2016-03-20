9a5d4883f655c1aad8514f18bf37e03f649f26ee
Peter Palfrader <peter@palfrader.org>
2016-03-20 16:02:35 +0100

    Retire portman and pittar (RT#6121, RT#6135)

--- modules/autofs/manifests/init.pp
+++ modules/autofs/manifests/init.pp
@@ -1,6 +1,6 @@
 class autofs {
 	case $::hostname {
-		pejacevic, piu-slave-bm-a, picconi, coccia, dillon, donizetti, ticharich, delfin, quantz, sor, lindsay, mekeel, pittar, pinel, tate: {
+		pejacevic, piu-slave-bm-a, picconi, coccia, dillon, donizetti, ticharich, delfin, quantz, sor, lindsay, mekeel, pinel, tate: {
 			include autofs::bytemark
 		}
 		lw07,lw08: {
