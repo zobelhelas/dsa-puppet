bc0339c24b44a4e78e82ff588d7bb0e9f665487c
Peter Palfrader <peter@palfrader.org>
2016-03-22 08:11:48 +0100

    renumber mirror-isc3

--- modules/roles/manifests/debug_mirror.pp
+++ modules/roles/manifests/debug_mirror.pp
@@ -1,7 +1,7 @@
 class roles::debug_mirror {
 	$vhost_listen = $::hostname ? {
 		klecker    => '130.89.148.14:80 [2001:610:1908:b000::148:14]:80',
-		mirror-isc => '149.20.20.20:80 [2001:4f8:8:36::1deb:20]:80',
+		mirror-isc => '149.20.20.22:80 [2001:4f8:8:36::1deb:22]:80',
 		default => '*:80',
 	}
 
