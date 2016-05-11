028da476cdb0458bd4fc6f91fdb7a83b44ac98a3
Peter Palfrader <peter@palfrader.org>
2016-05-11 22:34:08 +0200

    keyring.d.o cert on keyring service

--- modules/roles/manifests/keyring.pp
+++ modules/roles/manifests/keyring.pp
@@ -2,4 +2,9 @@ class roles::keyring {
 	rsync::site { 'keyring':
 		source => 'puppet:///modules/roles/keyring/rsyncd.conf',
 	}
+
+	ssl::service { 'keyring.debian.org':
+		notify => Service['apache2'],
+		key => true,
+	}
 }
