fe052293cfa41644eb5b7d8a9686032dd2fca61a
Peter Palfrader <peter@palfrader.org>
2016-05-26 00:50:01 +0200

    respighi autofs/nfs

--- modules/autofs/manifests/init.pp
+++ modules/autofs/manifests/init.pp
@@ -1,6 +1,6 @@
 class autofs {
 	case $::hostname {
-		pejacevic, piu-slave-bm-a, picconi, coccia, dillon, donizetti, ticharich, delfin, quantz, sor, lindsay, mekeel, pinel, tate, manziarly: {
+		pejacevic, piu-slave-bm-a, picconi, coccia, dillon, donizetti, ticharich, delfin, quantz, sor, lindsay, mekeel, pinel, tate, manziarly, respighi: {
 			include autofs::bytemark
 		}
 		lw07,lw08: {
