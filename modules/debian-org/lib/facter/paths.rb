fbe56e4206801c65dae31a524d1af8665ead5714
Peter Palfrader <peter@palfrader.org>
2016-05-26 22:34:32 +0200

    Add more -ports stuff

--- modules/debian-org/lib/facter/paths.rb
+++ modules/debian-org/lib/facter/paths.rb
@@ -4,6 +4,7 @@
    /etc/ssh/ssh_host_ed25519_key
    /srv/mirrors/debian
    /srv/mirrors/debian-debug
+   /srv/mirrors/debian-ports
    /srv/mirrors/debian-security
    /dev/hwrng
 }.each do |path|
