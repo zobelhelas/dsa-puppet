56f214ebc82a69b05595ff2107065cf770277430
Julien Cristau <jcristau@debian.org>
2016-05-28 15:20:39 +0200

    nfs server on senfter
    Signed-off-by: Julien Cristau <jcristau@debian.org>

--- manifests/site.pp
+++ manifests/site.pp
@@ -102,7 +102,7 @@ node default {
 	#	include dacs
 	#}
 
-	if $::hostname in [buxtehude,glinka,milanollo,lw01,lw02,lw03,lw04] {
+	if $::hostname in [buxtehude,glinka,milanollo,lw01,lw02,lw03,lw04,senfter] {
 		include nfs-server
 	}
 
