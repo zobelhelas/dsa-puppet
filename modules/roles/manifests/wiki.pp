ac5baa1e2ca6b36ccea25497ecb79c2c0d7528b3
Aurelien Jarno <aurelien@aurel32.net>
2016-07-01 20:14:42 +0200

    No TLSA for wiki.debian.org for a while
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

--- modules/roles/manifests/wiki.pp
+++ modules/roles/manifests/wiki.pp
@@ -1,6 +1,7 @@
 class roles::wiki {
 	ssl::service { 'wiki.debian.org':
 		notify => Service['apache2'],
+		tlsaport => [],
 	}
 	rsync::site { 'wiki':
 		source => 'puppet:///modules/roles/wiki/rsyncd.conf',
