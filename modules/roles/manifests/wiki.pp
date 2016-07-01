e907af2356704731fcaf16b7e00b655cd363f5f5
Aurelien Jarno <aurelien@aurel32.net>
2016-07-01 22:56:02 +0200

    Switch wiki.debian.org to letsencrypt
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

--- modules/roles/manifests/wiki.pp
+++ modules/roles/manifests/wiki.pp
@@ -1,7 +1,7 @@
 class roles::wiki {
 	ssl::service { 'wiki.debian.org':
 		notify => Service['apache2'],
-		tlsaport => [],
+		key => true,
 	}
 	rsync::site { 'wiki':
 		source => 'puppet:///modules/roles/wiki/rsyncd.conf',
