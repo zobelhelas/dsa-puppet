6b9e31db704fa673f4935c756a754a0e4cd4f10d
Julien Cristau <jcristau@debian.org>
2016-07-27 13:07:08 +0200

    Pin older hp-health on DL3*0 G5
    Latest hp-health doesn't seem to work on those boxes.
    
    Signed-off-by: Julien Cristau <jcristau@debian.org>

--- modules/hardware/manifests/init.pp
+++ modules/hardware/manifests/init.pp
@@ -5,6 +5,10 @@ class hardware {
 		file { '/etc/apt/preferences.d/dsa-hp-tools':
 			content => "Package: hp-health\nPin: version 8.6*\nPin-Priority: 1100\n"
 		}
+	} elsif "$::systemproductname" in ["ProLiant DL380 G5", "ProLiant DL360 G5"] {
+		file { '/etc/apt/preferences.d/dsa-hp-tools':
+			content => "Package: hp-health\nPin: version 10.0.*\nPin-Priority: 1100\n"
+		}
 	} else {
 		file { '/etc/apt/preferences.d/dsa-hp-tools':
 			ensure => absent
