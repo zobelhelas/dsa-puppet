4921ab7b2bc850505988c8137b35b86269e52c09
Peter Palfrader <peter@palfrader.org>
2016-07-28 20:32:22 +0000

    move to stringifying our facts again

--- modules/debian-org/lib/facter/onion-services.rb
+++ modules/debian-org/lib/facter/onion-services.rb
@@ -1,13 +1,19 @@
-Facter.add("onion_hostname") do
-	services = {}
+begin
+    require 'json'
 
-	Dir['/var/lib/tor/onion/*/hostname'].each do |p|
-		dir = File.dirname(p)
-		service = File.basename(dir)
-		hostname = IO.read(p).chomp
-		services[service] = hostname
-	end
-	setcode do
-		services
-	end
+    Facter.add("onion_hostname") do
+        services = {}
+
+        Dir['/var/lib/tor/onion/*/hostname'].each do |p|
+            dir = File.dirname(p)
+            service = File.basename(dir)
+            hostname = IO.read(p).chomp
+            services[service] = hostname
+        end
+        setcode do
+            services.to_json
+        end
+    end
+
+rescue Exception => e
 end
