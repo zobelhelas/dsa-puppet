e04f523b1744db8a97e530e8945ded4e2f046d6c
Peter Palfrader <peter@palfrader.org>
2016-07-28 20:32:47 +0000

    put an onion instance into a storedconf for onionbalance

--- modules/onion/manifests/service.pp
+++ modules/onion/manifests/service.pp
@@ -10,4 +10,13 @@ define onion::service (
 		order   => 10,
 		content => "HiddenServiceDir /var/lib/tor/onion/${name}\nHiddenServicePort ${port} ${target_address}:${target_port}\n\n",
 	}
+
+	$onion_hn = onion_hostname($name)
+	if $onion_hn {
+		@@concat::fragment { "onion::balance::instance::$name::$hostname":
+			target  => "/etc/onionbalance/config",
+			content => "      - address: ${onion_hn}\n        name: ${hostname}-${name}",
+			tag => "onion::balance::$name",
+		}
+	}
 }
