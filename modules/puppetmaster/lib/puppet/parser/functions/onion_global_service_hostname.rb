7428fc1d6b4b7a740d54cbbf456d5f6306572e3b
Peter Palfrader <peter@palfrader.org>
2016-07-29 16:49:59 +0000

    onion for www

---
+++ modules/puppetmaster/lib/puppet/parser/functions/onion_global_service_hostname.rb
@@ -0,0 +1,23 @@
+# This function returns the .onion name for a given service name on Debian's onionbalance instance
+# for this, it reads /srv/puppet.debian.org/puppet-facts/onionbalance-services.yaml
+# this comes from the onionbalance-host, via puppet's storedconf
+#
+module Puppet::Parser::Functions
+  newfunction(:onion_global_service_hostname, :type => :rvalue) do |args|
+    servicename = args.shift()
+
+    fn = '/srv/puppet.debian.org/puppet-facts/onionbalance-services.yaml'
+    unless File.exist?(fn)
+      return false
+    end
+
+    facts = IO.read(fn)
+
+    require 'json'
+    parsed = JSON.parse(facts)
+    return parsed[servicename]
+  end
+end
+# vim:set ts=2:
+# vim:set et:
+# vim:set shiftwidth=2:
