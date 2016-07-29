b3d1e16b4dabefa8e1c978f2cf2a697676cdf3cf
Peter Palfrader <peter@palfrader.org>
2016-07-29 19:52:01 +0000

    export onion list to dillon

--- modules/roles/manifests/static_master.pp
+++ modules/roles/manifests/static_master.pp
@@ -13,4 +13,17 @@ class roles::static_master {
 	file { '/etc/static-clients.conf':
 		content => template('roles/static-mirroring/static-clients.conf.erb'),
 	}
+
+	# export some information for the onion.debian.org build
+	if $::hostname in [dillon] {
+		file { '/srv/puppet.debian.org':
+			ensure => directory
+		}
+		file { '/srv/puppet.debian.org/puppet-facts':
+			ensure => directory
+		}
+		concat { '/srv/puppet.debian.org/puppet-facts/onionbalance-services.yaml':
+		}
+		Concat::Fragment <<| tag == "onionbalance-services.yaml" |>>
+	}
 }
