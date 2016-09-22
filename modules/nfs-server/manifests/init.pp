2548bab7123568c933610b94a4b49827cff4a057
Aurelien Jarno <aurelien@aurel32.net>
2016-09-23 00:14:19 +0200

    Update buxtehude and glinka NFS firewall
    Now that buxtehude is also on the private network, we can use it instead
    of the public IP. For that split the buxtehude and glinka configuration.
    
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

--- modules/nfs-server/manifests/init.pp
+++ modules/nfs-server/manifests/init.pp
@@ -23,8 +23,11 @@ class nfs-server {
 		milanollo,senfter: {
 			$client_range    = '172.29.122.0/24'
 		}
-		buxtehude,glinka: {
-			$client_range    = '(192.168.2.0/24 209.87.16.34/32 209.87.16.37/32 209.87.16.38/32)'
+		buxtehude: {
+			$client_range    = '(172.29.40.0/22 206.12.19.126/32)'
+		}
+		glinka: {
+			$client_range    = '(209.87.16.37/32 209.87.16.38/32)'
 		}
 		gretchaninov: {
 			$client_range    = '172.29.40.0/22'
