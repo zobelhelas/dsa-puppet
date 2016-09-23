c519bff9d7a0f549aef31b0a1cbaa51bb0b1804a
Peter Palfrader <peter@palfrader.org>
2016-09-23 21:48:52 +0200

    We have no lsbmajdistrelease <= 7 hosts anymore

--- modules/apache2/manifests/config.pp
+++ modules/apache2/manifests/config.pp
@@ -16,48 +16,30 @@ define apache2::config (
 		default: { fail ( "Unknown ensure value: '$ensure'" ) }
 	}
 
-	if $::lsbmajdistrelease <= 7 {
-		if $content {
-			file { "/etc/apache2/conf.d/${name}":
-				ensure  => $ensure,
-				content => $content,
-				require => Package['apache2'],
-				notify  => Exec['service apache2 reload'],
-			}
-		} else {
-			file { "/etc/apache2/conf.d/${name}":
-				ensure  => $ensure,
-				source  => $source,
-				require => Package['apache2'],
-				notify  => Exec['service apache2 reload'],
-			}
-		}
-	} else {
-		if $content {
-			file { "/etc/apache2/conf-available/${name}.conf":
-				ensure  => $ensure,
-				content => $content,
-				require => Package['apache2'],
-				notify  => Exec['service apache2 reload'],
-			}
-		} else {
-			file { "/etc/apache2/conf-available/${name}.conf":
-				ensure  => $ensure,
-				source  => $source,
-				require => Package['apache2'],
-				notify  => Exec['service apache2 reload'],
-			}
-		}
-
-		$link_ensure = $ensure ? {
-			present => link,
-			absent  => absent
-		}
-
-		file { "/etc/apache2/conf-enabled/${name}.conf":
-			ensure => $link_ensure,
-			target => "../conf-available/${name}.conf",
+	if $content {
+		file { "/etc/apache2/conf-available/${name}.conf":
+			ensure  => $ensure,
+			content => $content,
+			require => Package['apache2'],
 			notify  => Exec['service apache2 reload'],
 		}
+	} else {
+		file { "/etc/apache2/conf-available/${name}.conf":
+			ensure  => $ensure,
+			source  => $source,
+			require => Package['apache2'],
+			notify  => Exec['service apache2 reload'],
+		}
+	}
+
+	$link_ensure = $ensure ? {
+		present => link,
+		absent  => absent
+	}
+
+	file { "/etc/apache2/conf-enabled/${name}.conf":
+		ensure => $link_ensure,
+		target => "../conf-available/${name}.conf",
+		notify  => Exec['service apache2 reload'],
 	}
 }
