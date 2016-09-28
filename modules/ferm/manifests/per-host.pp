fba08a3dc949fa0cc12ae3fff387bc2f53e9a5cd
Julien Cristau <jcristau@debian.org>
2016-09-28 19:13:30 +0200

    rsync on gretchaninov

--- modules/ferm/manifests/per-host.pp
+++ modules/ferm/manifests/per-host.pp
@@ -3,7 +3,7 @@ class ferm::per-host {
 		include ferm::zivit
 	}
 
-	if $::hostname in [glinka] {
+	if $::hostname in [glinka,gretchaninov] {
 		ferm::rule { 'dsa-rsync':
 			domain      => '(ip ip6)',
 			description => 'Allow rsync access',
