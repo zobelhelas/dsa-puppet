a13971dba2d897bfd8059925aad8fd4c9477788c
Peter Palfrader <peter@palfrader.org>
2016-10-12 14:37:14 +0200

    LE cert for nm, contributors

--- modules/roles/manifests/contributors.pp
+++ modules/roles/manifests/contributors.pp
@@ -1,6 +1,6 @@
 class roles::contributors {
 	ssl::service { 'contributors.debian.org':
 		notify  => Exec['service apache2 reload'],
-		tlsaport => 0,
+		key => true,
 	}
 }
--- modules/roles/manifests/nm.pp
+++ modules/roles/manifests/nm.pp
@@ -1,6 +1,6 @@
 class roles::nm {
 	ssl::service { 'nm.debian.org':
 		notify  => Exec['service apache2 reload'],
-		tlsaport => 0,
+		key => true,
 	}
 }
