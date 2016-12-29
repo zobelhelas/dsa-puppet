a378dbd84f1d070367de280b01f540084ee3f60c
Tollef Fog Heen <tfheen@err.no>
2016-12-29 08:52:53 +0100

    Redirect stdout to /dev/null too

--- modules/debian-org/lib/facter/software.rb
+++ modules/debian-org/lib/facter/software.rb
@@ -157,6 +157,6 @@ Facter.add("systemd") do
 end
 Facter.add("tor_ge_0_2_9") do
 	setcode do
-		system(%{dpkg -l tor 2>/dev/null && dpkg --compare-versions $(dpkg-query -W -f='${Version}' tor) ge 0.2.9})
+		system(%{dpkg -l tor >/dev/null 2>&1 && dpkg --compare-versions $(dpkg-query -W -f='${Version}' tor) ge 0.2.9})
 	end
 end
