1af9be13609feffdbf52ed7277a08d1d3b3e230d
Peter Palfrader <peter@palfrader.org>
2017-01-27 22:01:45 +0100

    add bilbao

--- modules/autofs/manifests/init.pp
+++ modules/autofs/manifests/init.pp
@@ -1,6 +1,6 @@
 class autofs {
 	case $::hostname {
-		pejacevic, piu-slave-bm-a, picconi, coccia, dillon, donizetti, ticharich, delfin, quantz, sor, lindsay, mekeel, pinel, tate, manziarly, respighi: {
+		pejacevic, piu-slave-bm-a, picconi, coccia, dillon, donizetti, ticharich, delfin, quantz, sor, lindsay, mekeel, pinel, tate, manziarly, respighi, bilbao: {
 			include autofs::bytemark
 		}
 		lw07,lw08: {
