e9830a97cadd030be3c0ed1e2d312b4ceaedcca9
Peter Palfrader <peter@palfrader.org>
2017-01-31 21:43:44 +0100

    let new cdimage-search do https and onion

--- modules/roles/manifests/cdimage_search.pp
+++ modules/roles/manifests/cdimage_search.pp
@@ -1,10 +1,14 @@
 class roles::cdimage_search {
 	include apache2::proxy_http
+	include apache2::ssl
 
 	package { 'debian.org-cdimage-search.debian.org': ensure => installed, }
 
+	ssl::service { 'cdimage-search.debian.org': notify  => Exec['service apache2 reload'], key => true, }
 	apache2::site { '010-cdimage-search.debian.org':
 		site   => 'cdimage-search.debian.org',
 		content => template('roles/apache-cdimage-search.debian.org.conf.erb')
 	}
+
+	onion::service { 'cdimage-search.debian.org': port => 80, target_address => 'cdimage-search.debian.org', target_port => 80, direct => true }
 }
