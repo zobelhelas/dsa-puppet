591ca2bf4a911c072b8bc6aef2486765bb6d52f6
Bastian Blank <waldi@debian.org>
2017-02-10 21:00:01 +0100

    Use rsyncd via systemd on ftp_master

--- modules/roles/manifests/ftp_master.pp
+++ modules/roles/manifests/ftp_master.pp
@@ -1,12 +1,13 @@
 class roles::ftp_master {
-	rsync::site { 'dakmaster':
-		source        => 'puppet:///modules/roles/dakmaster/rsyncd.conf',
-		max_clients   => 100,
-		sslname       => 'ftp-master.debian.org',
+	rsync::site_systemd { 'dakmaster':
+		source      => 'puppet:///modules/roles/dakmaster/rsyncd.conf',
+		max_clients => 100,
+		sslname     => 'ftp-master.debian.org',
 	}
 
 	ssl::service { 'ftp-master.debian.org':
-		notify  => Exec['service apache2 reload'],
-		key => true,
+		notify   => Exec['service apache2 reload'],
+		key      => true,
+		tlsaport => [443, 1873],
 	}
 }
