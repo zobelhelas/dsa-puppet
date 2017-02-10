352faf50b6166b87fc6e7e6e723d266f8ad6f5ab
Peter Palfrader <peter@palfrader.org>
2017-02-10 21:36:35 +0100

    little /srv/mirrors changes

--- modules/roles/manifests/archvsync_base.pp
+++ modules/roles/manifests/archvsync_base.pp
@@ -1,15 +1,14 @@
 class roles::archvsync_base {
 	file { '/srv/mirrors':
 		ensure => directory,
-		owner  => 1176,
-		group  => 1176,
-		mode   => '0755',
-		links  => follow,
+		owner  => root,
+		group  => 1176, # archvsync
+		mode   => '0775',
+		# links  => follow,
 	}
 
 	file { '/srv/mirrors/.nobackup':
 		ensure  => present,
 		content => '',
-		mode    => '0444',
 	}
 }
