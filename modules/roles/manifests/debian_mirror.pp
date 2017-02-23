e28e1dc78726bf4780e5fcadb49653fe412de87d
Peter Palfrader <peter@palfrader.org>
2017-02-23 17:34:51 +0000

    fix debian mirror bind addr for ipv6 on klecker

--- modules/roles/manifests/debian_mirror.pp
+++ modules/roles/manifests/debian_mirror.pp
@@ -2,7 +2,7 @@ class roles::debian_mirror {
 	include roles::archvsync_base
 
 	$vhost_listen = $::hostname ? {
-		klecker    => '130.89.148.12:80 2001:610:1908:b000::148:12:80',
+		klecker    => '130.89.148.12:80 [2001:610:1908:b000::148:12]:80',
 		mirror-isc => '149.20.4.15:80 [2001:4f8:1:c::15]:80',
 		mirror-conova => '217.196.149.232:80 [2a02:16a8:dc41:100::232]:80',
 		default => '*:80',
