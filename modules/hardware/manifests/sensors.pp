27192017dd0fd3997953667ee93033b1c82b7eb8
Peter Palfrader <peter@palfrader.org>
2017-03-18 20:22:35 +0100

    make a reverse for hw_can_temp_sensors that removes lm-sensors

--- modules/hardware/manifests/sensors.pp
+++ modules/hardware/manifests/sensors.pp
@@ -2,5 +2,8 @@ class hardware::sensors {
 	if $::hw_can_temp_sensors {
 		package { 'lm-sensors': ensure => installed, }
 		munin::check { 'sensors_temp': script => 'sensors_' }
+	} else {
+		package { 'lm-sensors': ensure => purged, }
+		munin::check { 'sensors_temp': ensure => absent }
 	}
 }
