6b3f2197570ae91174b38c04329414c5166c8d05
Peter Palfrader <peter@palfrader.org>
2017-03-20 16:57:59 +0100

    remove debian.restricted.list apt source on hosts without proliant raid

--- modules/hardware/manifests/raid.pp
+++ modules/hardware/manifests/raid.pp
@@ -1,7 +1,5 @@
 class hardware::raid {
-	if $::smartarraycontroller {
-		include hardware::raid::proliant
-	}
+	include hardware::raid::proliant
 
 	if $::productname == 'PowerEdge 2850' {
 		include hardware::raid::megactl
--- modules/hardware/manifests/raid/proliant.pp
+++ modules/hardware/manifests/raid/proliant.pp
@@ -1,29 +1,34 @@
 class hardware::raid::proliant {
+	if $::smartarraycontroller {
+		site::aptrepo { 'debian.restricted':
+			url        => 'http://db.debian.org/debian-admin',
+			suite      => "${::lsbdistcodename}-restricted",
+			components => 'non-free',
+		}
 
-	site::aptrepo { 'debian.restricted':
-		url        => 'http://db.debian.org/debian-admin',
-		suite      => "${::lsbdistcodename}-restricted",
-		components => 'non-free',
-	}
-
-	package { 'hpacucli':
-		ensure  => installed,
-		tag    => extra_repo,
-	}
-	package { 'hpssacli':
-		ensure  => installed,
-		tag    => extra_repo,
-	}
-	if !("$::systemproductname" in ["ProLiant DL180 G5", "ProLiant DL120 G5", "ProLiant ML150 G5"]) {
-		package { 'hp-health':
-			ensure => installed,
+		package { 'hpacucli':
+			ensure  => installed,
 			tag    => extra_repo,
 		}
-	}
+		package { 'hpssacli':
+			ensure  => installed,
+			tag    => extra_repo,
+		}
+		if !("$::systemproductname" in ["ProLiant DL180 G5", "ProLiant DL120 G5", "ProLiant ML150 G5"]) {
+			package { 'hp-health':
+				ensure => installed,
+				tag    => extra_repo,
+			}
+		}
 
-	if $::debarchitecture == 'amd64' {
-		package { 'lib32gcc1':
-			ensure => installed,
+		if $::debarchitecture == 'amd64' {
+			package { 'lib32gcc1':
+				ensure => installed,
+			}
+		}
+	} else {
+		site::aptrepo { 'debian.restricted':
+			ensure => absent,
 		}
 	}
 }
