c6c43cfee660ee516de07f27f3cb7e6a3c8ce6d3
Bastian Blank <waldi@debian.org>
2017-03-31 17:17:37 +0200

    Use vsftpd via systemd on ftp_upload

--- modules/roles/manifests/ftp_upload.pp
+++ modules/roles/manifests/ftp_upload.pp
@@ -2,7 +2,7 @@ class roles::ftp_upload {
 
 	include ferm::ftp_conntrack
 
-	vsftpd::site { 'ftp-upload':
+	vsftpd::site_systemd { 'ftp-upload':
 		banner     => 'ftp.upload.debian.org FTP server',
 		logfile    => '/var/log/ftp/vsftpd-ftp.upload.debian.org.log',
 		writable   => true,
