5a376dd9e2986cd0b8c5c481f4c2e276b521798f
Bastian Blank <waldi@debian.org>
2017-03-31 15:41:22 +0200

    Use vsftpd::site_systemd on ports_master

--- modules/roles/manifests/ports_master.pp
+++ modules/roles/manifests/ports_master.pp
@@ -11,23 +11,12 @@ class roles::ports_master {
 
 	include ferm::ftp_conntrack
 
-	vsftpd::site { 'ports-master':
+	vsftpd::site_systemd { 'ports-master':
 		banner         => 'ports-master.debian.org FTP server',
 		logfile        => '/var/log/ftp/vsftpd-ports-master.debian.org.log',
 		writable       => true,
 		writable_other => true,
 		chown_user     => mini-dak-unpriv,
 		root           => '/srv/ports-master.debian.org/ftp.upload',
-	}
-
-	if $bind6 != '' {
-		vsftpd::site { 'ports-master-v6':
-			banner         => 'ports-master.debian.org FTP server',
-			logfile        => '/var/log/ftp/vsftpd-ports-master.debian.org.log',
-			writable       => true,
-			writable_other => true,
-			chown_user     => mini-dak-unpriv,
-			root           => '/srv/ports-master.debian.org/ftp.upload',
-		}
 	}
 }
