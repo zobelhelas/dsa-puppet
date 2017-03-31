1d4dd6ac681d38818f71dd2aecda0562a32dfca2
Bastian Blank <waldi@debian.org>
2017-03-31 18:33:45 +0200

    Use vsftpd via systemd on ftp
    We also only need a list of bind addresses, so merge them.

--- modules/roles/manifests/ftp.pp
+++ modules/roles/manifests/ftp.pp
@@ -3,31 +3,16 @@ class roles::ftp {
 
 	include ferm::ftp_conntrack
 
-	$bind = $::hostname ? {
-		klecker => '::ffff:130.89.148.12',
-		default => '',
+	$binds = $::hostname ? {
+		klecker => [ '130.89.148.12', '[2001:610:1908:b000::148:12]' ],
+		default => [ '[::]' ],
 	}
 
-	$bind6 = $::hostname ? {
-		klecker => '2001:610:1908:b000::148:12',
-		default => '',
-	}
-
-	vsftpd::site { 'ftp':
+	vsftpd::site_systemd { 'ftp':
 		banner       => 'ftp.debian.org FTP server',
 		logfile      => '/var/log/ftp/vsftpd-ftp.debian.org.log',
-		bind         => $bind,
+		binds        => $binds,
 		max_clients  => 200,
 		root         => '/srv/ftp.debian.org/ftp.root',
-	}
-
-	if $bind6 != '' {
-		vsftpd::site { 'ftp-v6':
-			banner       => 'ftp.debian.org FTP server',
-			logfile      => '/var/log/ftp/vsftpd-ftp.debian.org.log',
-			bind         => $bind6,
-			max_clients  => 200,
-			root         => '/srv/ftp.debian.org/ftp.root',
-		}
 	}
 }
