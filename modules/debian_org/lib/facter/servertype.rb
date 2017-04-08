801dbab607d7ea046f89db2d3fe553ebfda0d228
Aurelien Jarno <aurelien@aurel32.net>
2017-04-08 10:59:23 +0200

    Fix kvmdomain facter
    QEMU can return a CPU model different than "QEMU Virtual CPU". Check for
    the "hypervisor" flag instead.
    
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

--- modules/debian_org/lib/facter/servertype.rb
+++ modules/debian_org/lib/facter/servertype.rb
@@ -1,7 +1,7 @@
 Facter.add("kvmdomain") do
 	setcode do
 		result = false
-		if File.new('/proc/cpuinfo').read().index('QEMU Virtual CPU')
+		if File.new('/proc/cpuinfo').read().index('hypervisor')
 			result = true
 		end
 		result
